94ce07929ef11b839ddce732949dc37b2d55da68
DavidAce <aceituno@kth.se>
2019-11-30 16:42:50 +0100

    Fixing testing after build

--- tests/all-tests.cmake
+++ tests/all-tests.cmake
@@ -7,31 +7,36 @@ if(ENABLE_TESTS)
     if(DEPENDENCIES_FOUND)
         enable_testing()
         # Set up a target "all-tests" which will contain all the tests.
-        add_subdirectory(tests/simpleWrite          EXCLUDE_FROM_ALL)
-        add_subdirectory(tests/largeWrite           EXCLUDE_FROM_ALL)
-        add_subdirectory(tests/overWrite            EXCLUDE_FROM_ALL)
-        add_subdirectory(tests/readWrite            EXCLUDE_FROM_ALL)
-        add_subdirectory(tests/readWriteAttributes  EXCLUDE_FROM_ALL)
-        add_subdirectory(tests/copySwap             EXCLUDE_FROM_ALL)
-
-        add_custom_target(all-tests ALL
-                DEPENDS
-                test-simpleWrite
-                test-largeWrite
-                test-overWrite
-                test-readWrite
-                test-readWriteAttributes
-                test-copySwap
-                )
+#        add_subdirectory(tests/simpleWrite          EXCLUDE_FROM_ALL)
+#        add_subdirectory(tests/largeWrite           EXCLUDE_FROM_ALL)
+#        add_subdirectory(tests/overWrite            EXCLUDE_FROM_ALL)
+#        add_subdirectory(tests/readWrite            EXCLUDE_FROM_ALL)
+#        add_subdirectory(tests/readWriteAttributes  EXCLUDE_FROM_ALL)
+#        add_subdirectory(tests/copySwap             EXCLUDE_FROM_ALL)
+        add_subdirectory(tests/simpleWrite          )
+        add_subdirectory(tests/largeWrite           )
+        add_subdirectory(tests/overWrite            )
+        add_subdirectory(tests/readWrite            )
+        add_subdirectory(tests/readWriteAttributes  )
+        add_subdirectory(tests/copySwap             )
+#        add_custom_target(all-tests ALL
+#                DEPENDS
+#                test-simpleWrite
+#                test-largeWrite
+#                test-overWrite
+#                test-readWrite
+#                test-readWriteAttributes
+#                test-copySwap
+#                )
 
         if(ENABLE_TESTS_POST_BUILD)
             #Run all tests as soon as the tests have been built
-            add_custom_command(TARGET all-tests
-                    POST_BUILD
-                    COMMENT "Running Tests"
-                    DEPENDS h5pp deps flags
-                    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-                    COMMAND ${CMAKE_CTEST_COMMAND} -C $<CONFIGURATION> --output-on-failures)
+#            add_custom_command(TARGET all-tests
+#                    POST_BUILD
+#                    COMMENT "Running Tests"
+#                    DEPENDS h5pp deps flags
+#                    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+#                    COMMAND ${CMAKE_CTEST_COMMAND} -C $<CONFIGURATION> --output-on-failures)
         endif()
 
 #
