216f5f9e1218589e33c9a714063f42dfa3431fde
DavidAce <aceituno@kth.se>
2019-11-30 16:22:20 +0100

    Fixing testing after build

--- tests/all-tests.cmake
+++ tests/all-tests.cmake
@@ -14,25 +14,24 @@ if(ENABLE_TESTS)
         add_subdirectory(tests/readWriteAttributes  EXCLUDE_FROM_ALL)
         add_subdirectory(tests/copySwap             EXCLUDE_FROM_ALL)
 
-        add_custom_target(all-tests)
-        add_dependencies(all-tests test-simpleWrite        )
-        add_dependencies(all-tests test-largeWrite         )
-        add_dependencies(all-tests test-overWrite          )
-        add_dependencies(all-tests test-readWrite          )
-        add_dependencies(all-tests test-readWriteAttributes)
-        add_dependencies(all-tests test-copySwap           )
-
+        add_custom_target(all-tests ALL
+                DEPENDS
+                test-simpleWrite
+                test-largeWrite
+                test-overWrite
+                test-readWrite
+                test-readWriteAttributes
+                test-copySwap
+                )
 
         if(ENABLE_TESTS_POST_BUILD)
             #Run all tests as soon as the tests have been built
             add_custom_command(TARGET all-tests
                     POST_BUILD
                     COMMENT "Running Tests"
+                    DEPENDS h5pp deps flags
                     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
                     COMMAND ${CMAKE_CTEST_COMMAND} -C $<CONFIGURATION> --output-on-failures)
-
-            # Make sure the tests are built and run before building the main project
-            add_dependencies(${PROJECT_NAME} all-tests)
         endif()
 
 #
