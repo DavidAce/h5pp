8b665d429ed3dfe43d702d9fdaaf4d474099cc8a
DavidAce <aceituno@kth.se>
2019-12-05 15:23:32 +0100

    Updated testing to include checks of scalar2 and scalar3

--- tests/readWrite/readWrite.cpp
+++ tests/readWrite/readWrite.cpp
@@ -32,99 +32,84 @@ int main()
     h5pp::File file(outputFilename,h5pp::AccessMode::READWRITE, h5pp::CreateMode::TRUNCATE,logLevel);
 
 
+    std::vector<int> emptyVector;
+    file.writeDataset(emptyVector,"emptyVector");
+    file.readDataset(emptyVector,"emptyVector");
+
+
     // Generate dummy data
     std::string stringDummy = "Dummy string with spaces";
+    std::cout << "Writing stringDummy       : \n" <<  stringDummy    << std::endl;
+    // Write data data
+    file.writeDataset(stringDummy, "stringDummy");
+    std::cout << "Reading stringDummy: \n";
+    // Read the data back
+    std::string               stringDummyRead;
+    file.readDataset(stringDummyRead, "stringDummy");
+    std::cout << stringDummyRead << std::endl;
+    // Compare result
+    if (stringDummy != stringDummyRead){throw std::runtime_error("stringDummy != stringDummyRead");}
 
 
+
+    // std::vector<double>
     std::vector<double> vectorDouble = { 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0,
                                     0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0,
                                     -1.0, 0.0, 1.0, 0.0,-1.0, 0.0, 0.0,-1.0, 0.0, 1.0, 0.0, 1.0};
-    std::vector<cplx> vectorComplex = {-0.191154 + 0.326211i, 0.964728-0.712335i, -0.0351791-0.10264i,0.177544+0.99999i};
-
-    Eigen::MatrixXd matrixDouble(3,2);
-    matrixDouble.setRandom();
-
-    Eigen::Matrix<int,2,2,Eigen::RowMajor> matrixIntRowMajor;
-    matrixIntRowMajor.setRandom();
-
-    Eigen::Tensor<cplx,4> tensorComplex(2,3,2,3);
-    tensorComplex.setRandom();
-    Eigen::Tensor<double,3> tensorDoubleRowMajor(2,3,4);
-    tensorDoubleRowMajor.setRandom();
-
-
-    std::cout << "Writing stringDummy       : \n" <<  stringDummy    << std::endl;
-    file.writeDataset(stringDummy, "stringDummy");
-
     std::cout << "Writing vectorDouble      : \n" <<  vectorDouble   << std::endl;
     file.writeDataset(vectorDouble, "vectorDouble");
-
-    std::cout << "Writing vectorComplex     : \n" <<  vectorComplex  << std::endl;
-    file.writeDataset(vectorComplex, "vectorComplex");
-
-    std::cout << "Writing matrixDouble     : \n" <<  matrixDouble  << std::endl;
-    file.writeDataset(matrixDouble,{matrixDouble.rows(), matrixDouble.cols()}, "matrixDouble");
-
-
-    std::cout << "Writing matrixIntRowMajor     : \n" <<  matrixIntRowMajor  << std::endl;
-    file.writeDataset(matrixIntRowMajor,{matrixIntRowMajor.rows(), matrixIntRowMajor.cols()} , "matrixIntRowMajor");
-
-
-    std::cout << "Writing tensorComplex     : \n" <<  tensorComplex  << std::endl;
-    file.writeDataset(tensorComplex, "tensorComplex");
-
-    std::cout << "Writing tensorDoubleRowMajor     : \n" <<  tensorDoubleRowMajor  << std::endl;
-    file.writeDataset(tensorDoubleRowMajor, "tensorDoubleRowMajor");
-
-
-    auto foundLinksInRoot = file.getContentsOfGroup("/");
-    for (auto & link : foundLinksInRoot){
-        std::cout << "Found Link: " << link << std::endl;
-    }
-
-    // Read the data back
-    std::string               stringDummyRead;
-
-    std::vector<double>     vectorDoubleRead;
-    std::vector<cplx>       vectorComplexRead;
-
-    Eigen::MatrixXd matrixDoubleRead;
-    Eigen::Matrix<int,2,2,Eigen::RowMajor> matrixIntRowMajorRead;
-
-    Eigen::Tensor<cplx,4>     tensorComplexRead;
-    Eigen::Tensor<double,3>   tensorDoubleRowMajorRead;
-
-
-    std::cout << "Reading stringDummy: \n";
-    file.readDataset(stringDummyRead, "stringDummy");
-    std::cout << stringDummyRead << std::endl;
-    if (stringDummy != stringDummyRead){throw std::runtime_error("stringDummy != stringDummyRead");}
-
     std::cout << "Reading vectorDouble: \n";
-    file.readDataset(vectorDoubleRead, "vectorDouble");
+    auto vectorDoubleRead = file.readDataset<std::vector<double>>("vectorDouble");
     std::cout << vectorDoubleRead << std::endl;
     if (vectorDouble != vectorDoubleRead){throw std::runtime_error("vectorDouble != vectorDoubleRead");}
 
+
+    // std::vector<std::complex<double>>
+    std::vector<cplx> vectorComplex = {-0.191154 + 0.326211i, 0.964728-0.712335i, -0.0351791-0.10264i,0.177544+0.99999i};
+    std::cout << "Writing vectorComplex     : \n" <<  vectorComplex  << std::endl;
+    file.writeDataset(vectorComplex, "vectorComplex");
+    std::vector<cplx>       vectorComplexRead;
     std::cout << "Reading vectorComplex: \n";
     file.readDataset(vectorComplexRead, "vectorComplex");
     std::cout << vectorComplexRead << std::endl;
     if (vectorComplex != vectorComplexRead){throw std::runtime_error("vectorComplex != vectorComplexRead");}
 
+
+
+
+    // Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic, Eigen::ColMajor>
+    Eigen::MatrixXd matrixDouble(3,2);
+    matrixDouble.setRandom();
+    std::cout << "Writing matrixDouble     : \n" <<  matrixDouble  << std::endl;
+    file.writeDataset(matrixDouble,{matrixDouble.rows(), matrixDouble.cols()}, "matrixDouble");
+    Eigen::MatrixXd matrixDoubleRead;
     std::cout << "Reading matrixDouble: \n";
     file.readDataset(matrixDoubleRead, "matrixDouble");
     std::cout << matrixDoubleRead << std::endl;
     if (matrixDouble != matrixDoubleRead){throw std::runtime_error("matrixDouble != matrixDoubleRead");}
 
 
-    std::cout << "Reading matrixIntRowMajor: \n";
-    file.readDataset(matrixIntRowMajorRead, "matrixIntRowMajor");
-    std::cout << matrixIntRowMajorRead << std::endl;
-    if (matrixIntRowMajor != matrixIntRowMajorRead){throw std::runtime_error("matrixIntRowMajor != matrixIntRowMajorRead");}
+
+    // Eigen::Matrix<size_t,Eigen::Dynamic,Eigen::Dynamic, Eigen::Rowmajor>
+    Eigen::Matrix<size_t,2,2,Eigen::RowMajor> matrixSizeTRowMajor;
+    matrixSizeTRowMajor.setRandom();
+    std::cout << "Writing matrixSizeTRowMajor: \n" <<  matrixSizeTRowMajor  << std::endl;
+    file.writeDataset(matrixSizeTRowMajor,{matrixSizeTRowMajor.rows(), matrixSizeTRowMajor.cols()} , "matrixSizeTRowMajor");
+    std::cout << "Reading matrixSizeTRowMajor: \n";
+    Eigen::Matrix<size_t,2,2,Eigen::RowMajor> matrixSizeTRowMajorRead;
+    file.readDataset(matrixSizeTRowMajorRead, "matrixSizeTRowMajor");
+    std::cout << matrixSizeTRowMajorRead << std::endl;
+    if (matrixSizeTRowMajor != matrixSizeTRowMajorRead){throw std::runtime_error("matrixSizeTRowMajor != matrixSizeTRowMajorRead");}
 
 
 
 
+    Eigen::Tensor<cplx,4> tensorComplex(2,3,2,3);
+    tensorComplex.setRandom();
+    std::cout << "Writing tensorComplex     : \n" <<  tensorComplex  << std::endl;
+    file.writeDataset(tensorComplex, "tensorComplex");
     std::cout << "Reading tensorComplex: \n";
+    Eigen::Tensor<cplx,4>     tensorComplexRead;
     file.readDataset(tensorComplexRead, "tensorComplex");
     //Tensor comparison isn't as straightforward if we want to properly test storage orders
     Eigen::Map<Eigen::VectorXcd> tensorMap(tensorComplex.data(), tensorComplex.size());
@@ -140,10 +125,18 @@ int main()
             }
         }
     }
-
     if (tensorMap != tensorMapRead){throw std::runtime_error("tensorComplex != tensorComplexRead");}
 
 
+
+    Eigen::Tensor<double,3> tensorDoubleRowMajor(2,3,4);
+    tensorDoubleRowMajor.setRandom();
+
+
+
+    std::cout << "Writing tensorDoubleRowMajor: \n" <<  tensorDoubleRowMajor  << std::endl;
+    file.writeDataset(tensorDoubleRowMajor, "tensorDoubleRowMajor");
+    Eigen::Tensor<double,3>   tensorDoubleRowMajorRead;
     std::cout << "Reading tensorDoubleRowMajor: \n";
     file.readDataset(tensorDoubleRowMajorRead, "tensorDoubleRowMajor");
     //Tensor comparison isn't as straightforward if we want to properly test storage orders
@@ -158,14 +151,25 @@ int main()
             std::cout << std::endl;
         }
     }
-
     if (tensorMap2 != tensorMapRead2){throw std::runtime_error("tensorDoubleRowMajor != tensorDoubleRowMajorRead");}
 
 
-    auto dims        = file.getDatasetDims("vectorDouble");
-    auto * preAllocatedDouble = new double [dims[0]];
-    file.readDataset(preAllocatedDouble,dims[0], "vectorDouble");
-    delete[] preAllocatedDouble;
+
+    auto foundLinksInRoot = file.getContentsOfGroup("/");
+    for (auto & link : foundLinksInRoot){
+        std::cout << "Found Link: " << link << std::endl;
+    }
+
+
+    auto * cStyleDoubleArray = new double [10];
+    for(int i = 0; i < 10; i++) cStyleDoubleArray[i] = (double)i;
+    file.writeDataset(cStyleDoubleArray,10,"cStyleDoubleArray");
+    auto * cStyleDoubleArrayRead = new double [10];
+    file.readDataset(cStyleDoubleArrayRead,10, "cStyleDoubleArray");
+    delete[] cStyleDoubleArray;
+    delete[] cStyleDoubleArrayRead;
+
+
 
 
     // Test new field2 type
@@ -173,10 +177,18 @@ int main()
         double x ;
         double y ;
     };
-    std::vector<Field2> field2array (10,{0.3,0.8});
+    std::vector<Field2> field2array (10);
+    for(size_t i = 0; i < field2array.size();i++){
+        field2array[i].x = 2.3*i;
+        field2array[i].y = 20.5*i;
+    }
     file.writeDataset(field2array, "field2array");
     auto field2ReadArray = file.readDataset<std::vector<Field2>>("field2array");
-    for(auto &elem:field2ReadArray) std::cout << "elem: " << elem.x << " " << elem.y << std::endl;
+    for(size_t i= 0; i < field2array.size();i++) {
+        if(field2array[i].x != field2ReadArray[i].x) throw std::runtime_error("field2array != field2ReadArray at elem: " + std::to_string(i));
+        if(field2array[i].y != field2ReadArray[i].y) throw std::runtime_error("field2array != field2ReadArray at elem: " + std::to_string(i));
+    }
+
 
     Eigen::Matrix<Field2,Eigen::Dynamic,Eigen::Dynamic> field2Matrix (10,10);
     for(int row = 0; row < field2Matrix.rows(); row++){
@@ -185,18 +197,37 @@ int main()
         }
     }
     file.writeDataset(field2Matrix, "field2Matrix");
+    auto field2MatrixRead = file.readDataset<Eigen::Matrix<Field2,Eigen::Dynamic,Eigen::Dynamic> >("field2Matrix");
+    for(long i= 0; i < field2Matrix.cols();i++) {
+        for(long j= 0; j < field2Matrix.rows();j++) {
+            if(field2Matrix(i,j).x != field2MatrixRead(i,j).x) throw std::runtime_error("field2array != field2ReadArray at elem: " + std::to_string(i) +" "+ std::to_string(j));
+            if(field2Matrix(i,j).y != field2MatrixRead(i,j).y) throw std::runtime_error("field2array != field2ReadArray at elem: " + std::to_string(i) +" "+ std::to_string(j));
+        }
+    }
+
+//    if(field2Matrix != field2MatrixRead) throw std::runtime_error("field2Matrix != field2MatrixRead");
+
 
     // Test new field3 type
-
     struct Field3{
         double x ;
         double y ;
         double z ;
     };
-    std::vector<Field3> field3array (10,{0.3,0.8,0.5});
+    std::vector<Field3> field3array (10);
+    for(size_t i = 0; i < field3array.size();i++){
+        field3array[i].x = 2.3*i;
+        field3array[i].y = 20.5*i;
+        field3array[i].z = 200.9*i;
+    }
     file.writeDataset(field3array, "field3array");
     auto field3ReadArray = file.readDataset<std::vector<Field3>>("field3array");
-    for(auto &elem:field3ReadArray) std::cout << "elem: " << elem.x << " " << elem.y << " " << elem.z << std::endl;
+    for(size_t i= 0; i < field3array.size();i++) {
+        if(field3array[i].x != field3ReadArray[i].x) throw std::runtime_error("field3array.x != field3ReadArray.x at elem: " + std::to_string(i));
+        if(field3array[i].y != field3ReadArray[i].y) throw std::runtime_error("field3array.y != field3ReadArray.y at elem: " + std::to_string(i));
+        if(field3array[i].z != field3ReadArray[i].z) throw std::runtime_error("field3array.z != field3ReadArray.z at elem: " + std::to_string(i));
+    }
+
 
     Eigen::Matrix<Field3,Eigen::Dynamic,Eigen::Dynamic> field3Matrix (10,10);
     for(int row = 0; row < field3Matrix.rows(); row++){
@@ -205,6 +236,14 @@ int main()
         }
     }
     file.writeDataset(field3Matrix, "field3Matrix");
+    auto field3MatrixRead = file.readDataset<Eigen::Matrix<Field3,Eigen::Dynamic,Eigen::Dynamic>>("field3Matrix");
+    for(long i = 0; i < field3Matrix.cols(); i++) {
+        for(long j= 0; j < field3Matrix.rows(); j++) {
+            if(field3Matrix(i,j).x != field3MatrixRead(i,j).x) throw std::runtime_error("field3Matrix.x != field3MatrixRead.x at elem: " + std::to_string(i) +" "+ std::to_string(j));
+            if(field3Matrix(i,j).y != field3MatrixRead(i,j).y) throw std::runtime_error("field3Matrix.y != field3MatrixRead.y at elem: " + std::to_string(i) +" "+ std::to_string(j));
+            if(field3Matrix(i,j).z != field3MatrixRead(i,j).z) throw std::runtime_error("field3Matrix.z != field3MatrixRead.z at elem: " + std::to_string(i) +" "+ std::to_string(j));
+        }
+    }
 
 
 
