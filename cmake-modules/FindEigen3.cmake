c98d6d26e9fb0e9db38699fc540a7e265dd18263
DavidAce <aceituno@kth.se>
2019-12-14 00:11:24 +0100

    Rewrote FindEigen3.cmake to allow easier override and to let it define targets in one go

--- cmake-modules/FindEigen3.cmake
+++ cmake-modules/FindEigen3.cmake
@@ -107,7 +107,7 @@ if(NOT TARGET Eigen3::Eigen OR NOT EIGEN3_INCLUDE_DIR AND NOT EIGEN3_CONFIG_ONLY
         _eigen3_check_version()
     endif()
     if(EIGEN3_INCLUDE_DIR AND EIGEN3_VERSION_OK)
-        add_library(Eigen3::Eigen INTERFACE GLOBAL)
+        add_library(Eigen3::Eigen INTERFACE)
         set(Eigen3_FOUND TRUE)
         set_target_properties(Eigen3::Eigen PROPERTIES
                 INTERFACE_INCLUDE_DIRECTORIES "${EIGEN3_INCLUDE_DIR}")
