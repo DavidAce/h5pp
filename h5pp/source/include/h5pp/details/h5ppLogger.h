82e88a0e136d2f719c4ce29c43f0f5ec78284766
DavidAce <aceituno@kth.se>
2019-12-14 01:19:15 +0100

    Fixed bundled ranges in case fmt is external

--- h5pp/source/include/h5pp/details/h5ppLogger.h
+++ h5pp/source/include/h5pp/details/h5ppLogger.h
@@ -6,7 +6,12 @@
 #define H5PP_LOGGER_H
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/spdlog.h>
+
+#if __has_include(<spdlog/fmt/bundled/ranges.h>)
 #include <spdlog/fmt/bundled/ranges.h>
+#else
+#include <fmt/ranges.h>
+#endif
 
 namespace h5pp{
     namespace Logger{
