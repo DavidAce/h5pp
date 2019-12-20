08e21c3818b1d61d4037e7d703419ac3ce7a6e44
DavidAce <aceituno@kth.se>
2019-12-20 11:01:36 +0100

    Updated to print more properties on imported targets

--- cmake-modules/PrintTargetProperties.cmake
+++ cmake-modules/PrintTargetProperties.cmake
@@ -1,11 +1,12 @@
 # Get all propreties that cmake supports
 execute_process(COMMAND cmake --help-property-list OUTPUT_VARIABLE CMAKE_PROPERTY_LIST)
-
+list(APPEND CMAKE_PROPERTY_LIST IMPORTED_LOCATION IMPORTED_LOCATION_RELEASE IMPORTED_LOCATION_DEBUG)
 # Convert command output into a CMake list
 STRING(REGEX REPLACE ";" "\\\\;" CMAKE_PROPERTY_LIST "${CMAKE_PROPERTY_LIST}")
 STRING(REGEX REPLACE "\n" ";" CMAKE_PROPERTY_LIST "${CMAKE_PROPERTY_LIST}")
 # Fix https://stackoverflow.com/questions/32197663/how-can-i-remove-the-the-location-property-may-not-be-read-from-target-error-i
-list(FILTER CMAKE_PROPERTY_LIST EXCLUDE REGEX "^LOCATION$|^LOCATION_|_LOCATION$")
+#list(FILTER CMAKE_PROPERTY_LIST EXCLUDE REGEX "^LOCATION$|^LOCATION_|_LOCATION$")
+
 # For some reason, "TYPE" shows up twice - others might too?
 list(REMOVE_DUPLICATES CMAKE_PROPERTY_LIST)
 
@@ -17,14 +18,18 @@ foreach(prop ${CMAKE_PROPERTY_LIST})
     if(prop MATCHES "^(INTERFACE|[_a-z]|IMPORTED_LIBNAME_|MAP_IMPORTED_CONFIG_)|^(COMPATIBLE_INTERFACE_(BOOL|NUMBER_MAX|NUMBER_MIN|STRING)|EXPORT_NAME|IMPORTED(_GLOBAL|_CONFIGURATIONS|_LIBNAME)?|NAME|TYPE|NO_SYSTEM_FROM_IMPORTED)$")
         list(APPEND CMAKE_WHITELISTED_PROPERTY_LIST ${prop})
     endif()
-endforeach(prop)
+endforeach()
 
 function(print_properties)
-    message ("CMAKE_PROPERTY_LIST = ${CMAKE_PROPERTY_LIST}")
+    foreach(prop ${CMAKE_PROPERTY_LIST})
+        message(${prop})
+    endforeach()
 endfunction(print_properties)
 
 function(print_whitelisted_properties)
-    message ("CMAKE_WHITELISTED_PROPERTY_LIST = ${CMAKE_WHITELISTED_PROPERTY_LIST}")
+    foreach(prop ${CMAKE_WHITELISTED_PROPERTY_LIST})
+        message(${prop})
+    endforeach()
 endfunction(print_whitelisted_properties)
 
 function(print_target_properties tgt)
@@ -34,7 +39,7 @@ function(print_target_properties tgt)
     endif()
 
     get_target_property(target_type ${tgt} TYPE)
-    if(target_type STREQUAL "INTERFACE_LIBRARY")
+    if(target_type MATCHES "INTERFACE" OR target_type MATCHES "EXECUTABLE")
         set(PROP_LIST ${CMAKE_WHITELISTED_PROPERTY_LIST})
     else()
         set(PROP_LIST ${CMAKE_PROPERTY_LIST})
