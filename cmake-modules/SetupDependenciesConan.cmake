2a523c9f62ac2396b9d2612773f51f62e87f3e41
DavidAce <aceituno@kth.se>
2019-12-29 14:03:53 +0100

    Added Conan as an option for getting dependencies

---
+++ cmake-modules/SetupDependenciesConan.cmake
@@ -0,0 +1,79 @@
+
+##################################################################
+### Install conan-modules/conanfile.txt dependencies          ###
+### This uses conan to get spdlog/eigen3/h5pp/ceres           ###
+###    eigen/3.3.7@conan/stable                               ###
+###    spdlog/1.4.2@bincrafters/stable                        ###
+###    hdf5/1.10.5                                            ###
+##################################################################
+
+find_program (
+        CONAN_COMMAND
+        conan
+        HINTS ${CONAN_PREFIX} ${CONDA_PREFIX} $ENV{CONAN_PREFIX} $ENV{CONDA_PREFIX}
+        PATHS $ENV{HOME}/anaconda3 $ENV{HOME}/miniconda $ENV{HOME}/.conda
+        PATH_SUFFIXES bin envs/dmrg/bin
+)
+
+include(cmake-modules/conan/conan.cmake)
+conan_cmake_run(CONANFILE cmake-modules/conan/conanfile.txt
+        CONAN_COMMAND ${CONAN_COMMAND}
+        SETTINGS compiler.cppstd=17
+        SETTINGS compiler.libcxx=libstdc++11
+        BASIC_SETUP CMAKE_TARGETS OUTPUT_QUIET
+        BUILD missing)
+
+
+
+
+if(TARGET CONAN_PKG::Eigen3)
+    target_link_libraries(deps INTERFACE CONAN_PKG::Eigen3)
+    get_target_property(EIGEN3_INCLUDE_DIR CONAN_PKG::Eigen3 INTERFACE_INCLUDE_DIRECTORIES)
+    list(APPEND H5PP_DIRECTORY_HINTS ${EIGEN3_INCLUDE_DIR})
+    list(APPEND TARGETS_FOUND CONAN_PKG::Eigen3)
+else()
+    list(APPEND TARGETS_NOT_FOUND CONAN_PKG::Eigen3)
+endif()
+
+
+if(TARGET CONAN_PKG::spdlog)
+    target_link_libraries(deps INTERFACE CONAN_PKG::spdlog)
+    get_target_property(SPDLOG_INCLUDE_DIR CONAN_PKG::spdlog INTERFACE_INCLUDE_DIRECTORIES)
+    list(APPEND H5PP_DIRECTORY_HINTS ${SPDLOG_INCLUDE_DIR})
+    list(APPEND TARGETS_FOUND CONAN_PKG::spdlog)
+else()
+    list(APPEND TARGETS_NOT_FOUND CONAN_PKG::spdlog)
+endif()
+
+
+if(TARGET CONAN_PKG::HDF5)
+    target_link_libraries(deps INTERFACE CONAN_PKG::HDF5)
+    get_target_property(HDF5_INCLUDE_DIR CONAN_PKG::HDF5 INTERFACE_INCLUDE_DIRECTORIES)
+    list(APPEND H5PP_DIRECTORY_HINTS ${HDF5_INCLUDE_DIR})
+    list(APPEND TARGETS_FOUND CONAN_PKG::HDF5)
+else()
+    list(APPEND TARGETS_NOT_FOUND CONAN_PKG::HDF5)
+endif()
+
+
+foreach(tgt ${TARGETS_NOT_FOUND})
+    message(STATUS "Dependency not found: [${tgt}] -- make sure to find and link to it manually before using h5pp")
+endforeach()
+
+if(TARGETS_NOT_FOUND)
+    set(ALL_TARGETS_FOUND FALSE)
+else()
+    set(ALL_TARGETS_FOUND TRUE)
+endif()
+
+
+##################################################################
+### Link all the things!                                       ###
+##################################################################
+target_link_libraries(deps INTERFACE CONAN_PKG::Eigen3)
+target_link_libraries(deps INTERFACE CONAN_PKG::spdlog)
+target_link_libraries(deps INTERFACE CONAN_PKG::HDF5)
+
+
+
+
---
+++ cmake-modules/SetupDependenciesManual.cmake
@@ -0,0 +1,55 @@
+
+
+include(${PROJECT_SOURCE_DIR}/cmake-modules/Fetch_Eigen3.cmake)
+include(${PROJECT_SOURCE_DIR}/cmake-modules/Fetch_spdlog.cmake)
+include(${PROJECT_SOURCE_DIR}/cmake-modules/Fetch_HDF5.cmake)
+
+if(TARGET Eigen3::Eigen)
+    target_link_libraries(deps INTERFACE Eigen3::Eigen)
+    get_target_property(EIGEN3_INCLUDE_DIR Eigen3::Eigen INTERFACE_INCLUDE_DIRECTORIES)
+    list(APPEND H5PP_DIRECTORY_HINTS ${EIGEN3_INCLUDE_DIR})
+    list(APPEND TARGETS_FOUND Eigen3::Eigen)
+else()
+    list(APPEND TARGETS_NOT_FOUND Eigen3::Eigen)
+endif()
+
+
+if(TARGET spdlog::spdlog)
+    target_link_libraries(deps INTERFACE spdlog::spdlog)
+    get_target_property(SPDLOG_INCLUDE_DIR spdlog::spdlog INTERFACE_INCLUDE_DIRECTORIES)
+    list(APPEND H5PP_DIRECTORY_HINTS ${SPDLOG_INCLUDE_DIR})
+    list(APPEND TARGETS_FOUND spdlog::spdlog)
+else()
+    list(APPEND TARGETS_NOT_FOUND spdlog::spdlog)
+endif()
+
+
+if(TARGET hdf5::hdf5)
+    target_link_libraries(deps INTERFACE hdf5::hdf5)
+    get_target_property(HDF5_INCLUDE_DIR hdf5::hdf5 INTERFACE_INCLUDE_DIRECTORIES)
+    list(APPEND H5PP_DIRECTORY_HINTS ${HDF5_INCLUDE_DIR})
+    list(APPEND TARGETS_FOUND hdf5)
+else()
+    list(APPEND TARGETS_NOT_FOUND hdf5::hdf5)
+endif()
+
+
+foreach(tgt ${TARGETS_NOT_FOUND})
+    message(STATUS "Dependency not found: [${tgt}] -- make sure to find and link to it manually before using h5pp")
+endforeach()
+
+if(TARGETS_NOT_FOUND)
+    set(ALL_TARGETS_FOUND FALSE)
+else()
+    set(ALL_TARGETS_FOUND TRUE)
+endif()
+
+
+##################################################################
+### Link all the things!                                       ###
+##################################################################
+target_link_libraries(deps INTERFACE Eigen3::Eigen)
+target_link_libraries(deps INTERFACE spdlog::spdlog)
+target_link_libraries(deps INTERFACE hdf5::hdf5)
+
+
