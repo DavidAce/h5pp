90bf9ec0a06d9eb22d732de313bd56330428aa2f
DavidAce <aceituno@kth.se>
2020-01-04 12:41:24 +0100

    Trying windows/macos builds

--- cmake/CheckTypeTraits.cmake
+++ cmake/CheckTypeTraits.cmake
@@ -1,18 +1,19 @@
 function(CheckTypeTraits)
     set(CMAKE_REQUIRED_FLAGS "-std=c++17")
+    list(APPEND CMAKE_REQUIRED_INCLUDES ${PROJECT_SOURCE_DIR}/h5pp/include/h5pp/details)
+
     include(CheckIncludeFileCXX)
     check_include_file_cxx(experimental/type_traits    has_type_traits  )
     if(NOT has_type_traits)
-        message(WARNING "\n\
+        message(WARNING "
                 Missing <experimental/type_traits> header.\n\
                 Consider using a newer compiler (GCC 8 or above, Clang 7 or above),\n\
                 or checking the compiler flags. If using Clang, try passing the variable \n\
                 GCC_TOOLCHAIN=<path> \n\
                 where path is the install directory of a recent GCC installation.
-                h5pp will try to mimic std::experimental::is_detected
+                h5pp will now try to mimic std::experimental::is_detected
         ")
     endif()
-    list(APPEND CMAKE_REQUIRED_INCLUDES ${PROJECT_SOURCE_DIR}/h5pp/include/h5pp/details)
     include(CheckCXXSourceCompiles)
     check_cxx_source_compiles("
         #include<vector>
