a47c67a6541420ac648408d4649cd0127d330059
DavidAce <aceituno@kth.se>
2020-03-06 13:35:08 +0100

    Minor update
    * Prefixed CMake variables with H5PP_
    * Added logging when creating and appending to tables
    * Updated hdf5 to 1.12.0 on native builds
    * Testing MPI compatibility
    * More messaging about hdf5 version
    * Fixed hdf5 shared-lib builds
    * Increased required cmake version to 3.12
    * Enabled cmake --parallel

--- cmake/BuildDependency.cmake
+++ cmake/BuildDependency.cmake
@@ -32,7 +32,7 @@ function(build_dependency dep_name install_dir extra_flags)
     endif()
 
 
-    execute_process(COMMAND  ${CMAKE_COMMAND} --build .
+    execute_process(COMMAND  ${CMAKE_COMMAND} --build . --parallel
             WORKING_DIRECTORY "${build_dir}"
             RESULT_VARIABLE build_result
             ERROR_VARIABLE  build_error
