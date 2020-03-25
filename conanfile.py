fa98cd9146f9eb8bf19ae20c35d20356cf571ac6
DavidAce <aceituno@kth.se>
2020-03-25 15:12:09 +0100

    Update to v1.6.0

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ import os, re
 
 class h5ppConan(ConanFile):
     name = "h5pp"
-    version = "1.5.4"
+    version = "1.6.0"
     license = "MIT"
     url = "https://github.com/DavidAce/h5pp"
     settings = "os", "compiler", "build_type", "arch"
