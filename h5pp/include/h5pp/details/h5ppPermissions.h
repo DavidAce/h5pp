e51130c82273844d9216f2b9b9975689a25b3e45
DavidAce <aceituno@kth.se>
2020-03-27 08:15:54 +0100

    Clarified file permissions

--- h5pp/include/h5pp/details/h5ppPermissions.h
+++ h5pp/include/h5pp/details/h5ppPermissions.h
@@ -18,11 +18,11 @@ namespace h5pp {
      | ---- | ---- | ---- | ---- |
      | `READONLY`                | Open with read-only permission       | Throw error     | Never writes to disk, fails if the file is not found |
      | `COLLISION_FAIL`          | Throw error                          | Create new file | Never deletes existing files and fails if it already exists |
-     | `RENAME` **(default)**    | Rename this file and create          | Create new file | Never deletes existing files, but invents a new filename to avoid collision by appending
-     "-#" (#=1,2,3...) to the stem of the filename | | `READWRITE`               | Open with read-write permission      | Create new file | Never deletes existing files, but is
-     allowed to open/modify | | `BACKUP`                  | Rename existing file and create      | Create new file | Avoids collision by backing up the existing file, appending
-     ".bak_#" (#=1,2,3...) to the filename | | `REPLACE`                 | Truncate (overwrite)                 | Create new file | Deletes the existing file and create a new one
-     in place |
+     | `RENAME` **(default)**    | Create renamed file                  | Create new file | Never deletes existing files, but invents a new filename to avoid collision by appending "-#" (#=1,2,3...) to the stem of the filename |
+     | `READWRITE`               | Open with read-write permission      | Create new file | Never deletes existing files, but is allowed to open/modify |
+     | `BACKUP`                  | Rename existing file and create new  | Create new file | Avoids collision by backing up the existing file, appending ".bak_#" (#=1,2,3...) to the filename |
+     | `REPLACE`                 | Truncate (overwrite)                 | Create new file | Deletes the existing file and create a new one in place |
+
 
      * When a new file is created, the intermediate directories are always created automatically.
      * When a new file is created, `READWRITE` permission to it is implied.
