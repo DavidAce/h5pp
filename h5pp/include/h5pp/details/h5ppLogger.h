c2af81369d30c0f19124c01288847a8bd551ca90
DavidAce <aceituno@kth.se>
2020-04-08 09:45:52 +0200

    Handling of formatting is done separately

--- h5pp/include/h5pp/details/h5ppLogger.h
+++ h5pp/include/h5pp/details/h5ppLogger.h
@@ -85,10 +85,17 @@ namespace h5pp::logger {
 
     inline void enableTimestamp() {}
     inline void disableTimestamp() {}
+    inline size_t getLogLevel(){
+        if(log != nullptr) return log->logLevel;
+        else return 2;
+    }
     template<typename levelType>
     inline void setLogLevel([[maybe_unused]] levelType levelZeroToFive) {
         if(log != nullptr) log->logLevel = levelZeroToFive;
     }
+//    inline size_t getLogLevel() {
+//        if(log != nullptr) return log->logLevel;
+//    }
     inline void setLogger([[maybe_unused]] const std::string & name_,
                           [[maybe_unused]] std::optional<int>  levelZeroToFive = std::nullopt,
                           [[maybe_unused]] std::optional<bool> timestamp       = std::nullopt) {
