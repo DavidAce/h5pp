156aecd421360139c7e9c727f9afbd4026891c01
DavidAce <aceituno@kth.se>
2020-04-08 09:38:35 +0200

    New test for chunking

---
+++ tests/chunkDimension/chunkDimension.cpp
@@ -0,0 +1,17 @@
+
+#include <complex>
+#include <h5pp/h5pp.h>
+#include <iostream>
+
+int main() {
+    // Define dummy data
+    std::string outputFilename = "output/overWrite.h5";
+    size_t      logLevel       = 0;
+    h5pp::File  file(outputFilename, h5pp::FilePermission::REPLACE, logLevel);
+
+    using namespace std::complex_literals;
+    std::vector<std::complex<double>> vectorComplexDouble(10000, {10.0, 5.0});
+
+    file.writeDataset(vectorComplexDouble, "overWriteGroup_ext_enabled/vectorComplexDouble_as_matrix");
+    return 0;
+}
