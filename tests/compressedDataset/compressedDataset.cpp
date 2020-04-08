2f923422a8b9805aa2929dfb5802908705e73be3
DavidAce <aceituno@kth.se>
2020-04-08 09:39:28 +0200

    New test for compression

---
+++ tests/compressedDataset/compressedDataset.cpp
@@ -0,0 +1,21 @@
+
+#include <complex>
+#include <h5pp/h5pp.h>
+#include <iostream>
+
+int main() {
+    std::string                       outputFilename = "output/compressedDataset.h5";
+    size_t                            logLevel       = 0;
+    h5pp::File                        file(outputFilename, h5pp::FilePermission::REPLACE, logLevel);
+
+#ifdef H5PP_EIGEN3
+    // Test compressed writes
+    if(h5pp::hdf5::checkIfCompressionIsAvailable()) {
+        file.setCompressionLevel(9);
+        Eigen::Tensor<double, 4> bigTensor(40, 180, 40, 5);
+        bigTensor.setConstant(1.0);
+        file.writeDataset(bigTensor, "compressedWriteGroup/bigTensor");
+    }
+#endif
+    return 0;
+}
