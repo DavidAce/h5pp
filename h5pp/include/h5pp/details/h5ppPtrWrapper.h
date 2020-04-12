b9cb07735b7e3eed6f9dfdcefb7ac7c9e9642978
DavidAce <aceituno@kth.se>
2020-04-12 14:37:19 +0200

    Copy vector elements in case they are not hsize_t

--- h5pp/include/h5pp/details/h5ppPtrWrapper.h
+++ h5pp/include/h5pp/details/h5ppPtrWrapper.h
@@ -39,7 +39,7 @@ namespace h5pp {
         }
 
         template<typename T>
-        PtrWrapper(const PointerType data, const std::vector<T> &dims) : data_(data), dims_(dims), size_(std::accumulate(dims.begin(), dims.end(), (T) 1, std::multiplies<>())) {
+        PtrWrapper(const PointerType data, const std::vector<T> &dims) : data_(data), dims_(dims.begin(), dims.end()), size_(std::accumulate(dims.begin(), dims.end(), (T) 1, std::multiplies<>())) {
             static_assert(std::is_integral_v<T> and "Type of dimension array must be integral");
             assert(N == dims_.size() and "Dimension mismatch");
         }
