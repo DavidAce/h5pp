686d97af4fa2d95bbb3c1a5dbde08924013668fc
DavidAce <aceituno@kth.se>
2020-04-25 13:15:46 +0200

    Build and run examples

---
+++ quickstart/linux-cmake-find-or-fetch-all-dependencies/step1-install-h5pp.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Get the source
+git clone https://github.com/DavidAce/h5pp.git
+
+# Make build directories
+cd h5pp
+mkdir -p build/Release
+cd build/Release
+
+
+# Run CMake configure (optionally do this with cmake-gui)
+# CMake version > 3.12 is required!
+# The default install directory will be "install" under this current directory.
+# For this example we use "../../install" instead.
+
+cmake -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_PREFIX=../../install \
+      -DBUILD_SHARED_LIBS=OFF \
+      -DH5PP_DOWNLOAD_METHOD=find-or-fetch \
+      -DH5PP_ENABLE_EIGEN3=ON \
+      -DH5PP_ENABLE_SPDLOG=ON \
+      -DH5PP_ENABLE_TESTS=ON \
+      -DH5PP_PRINT_INFO=ON \
+      ../../
+
+# Build the targets if any were selected.
+# In this case, it would only be the enabled tests.
+cmake --build . --parallel $(nproc)
+
+
+# Install the headers and other files to the location specified in CMAKE_INSTALL_PREFIX
+cmake --build . --target install
+
+# Run the tests to make sure everything is in order. This step is optional.
+ctest -C Debug --output-on-failure
---
+++ quickstart/linux-cmake-link-manual-all-dependencies/step1-install-h5pp.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Get the source
+git clone https://github.com/DavidAce/h5pp.git
+
+# Make build directories
+cd h5pp
+mkdir -p build/Release
+cd build/Release
+
+
+# Run CMake configure (optionally do this with cmake-gui)
+# CMake version > 3.12 is required!
+# The default install directory will be "install" under this current directory.
+# For this example we use "../../install" instead.
+
+cmake -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_PREFIX=../../install \
+      -DBUILD_SHARED_LIBS=OFF \
+      -DH5PP_DOWNLOAD_METHOD=none \
+      -DH5PP_ENABLE_EIGEN3=ON \
+      -DH5PP_ENABLE_SPDLOG=ON \
+      -DH5PP_ENABLE_TESTS=OFF \
+      -DH5PP_PRINT_INFO=ON \
+      ../../
+
+
+# Install the headers and other files to the location specified in CMAKE_INSTALL_PREFIX
+cmake --build . --target install
+
