ca2939a6efc8e301f3bb37d39f247ab4c7832193
DavidAce <aceituno@kth.se>
2020-05-01 10:59:32 +0200

    Simplified flags and download methods

--- cmake/Fetch_ghcFilesystem.cmake
+++ cmake/Fetch_ghcFilesystem.cmake
@@ -1,14 +1,12 @@
 
-find_package(ghcFilesystem HINTS ${ghcfilesystem_install_prefix} )
+find_package(ghcFilesystem HINTS ${CMAKE_INSTALL_PREFIX} PATH_SUFFIXES ghcFilesystem)
 
-
-if(TARGET ghcFilesystem::ghc_filesystem)
-    #    All done
-else ()
+if  (NOT TARGET ghcFilesystem::ghc_filesystem)
     message(STATUS "ghcFilesystem will be installed into ${CMAKE_INSTALL_PREFIX}")
     include(${PROJECT_SOURCE_DIR}/cmake/BuildDependency.cmake)
-    build_dependency(ghcFilesystem  "${ghcfilesystem_install_prefix}" "" "")
-    find_package(ghcFilesystem HINTS ${ghcfilesystem_install_prefix}
+    build_dependency(ghcFilesystem  "${CMAKE_INSTALL_PREFIX}" "" "")
+    find_package(ghcFilesystem HINTS ${CMAKE_INSTALL_PREFIX}
+                PATH_SUFFIXES ghcFilesystem
                 NO_DEFAULT_PATH NO_CMAKE_PACKAGE_REGISTRY )
 
     if(TARGET ghcFilesystem::ghc_filesystem)
