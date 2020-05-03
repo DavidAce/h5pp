b4de69cf9204bf34d7fa79d990e3836f3675e387
DavidAce <aceituno@kth.se>
2020-05-03 08:58:46 +0200

    Revised hint mechanism for search paths

--- cmake/SetupSearchPaths.cmake
+++ cmake/SetupSearchPaths.cmake
@@ -3,13 +3,18 @@ list(APPEND CMAKE_MODULE_PATH ${PROJECT_SOURCE_DIR}/cmake)
 list(APPEND CMAKE_PREFIX_PATH ${CMAKE_INSTALL_PREFIX}) # Works like HINTS but can be ignored by NO_DEFAULT_PATH NO_CMAKE_PATH and NO_CMAKE_ENVIRONMENT_PATH
 
 list(APPEND CMAKE_PREFIX_PATH
-        ${EIGEN3_DIRECTORY_HINTS}
+        ${H5PP_PREFIX_PATH}
+        ${EIGEN3_PREFIX_PATH}
+        ${SPDLOG_PREFIX_PATH}
+        ${HDF5_PREFIX_PATH}
+        ${H5PP_DIRECTORY_HINTS}
         ${SPDLOG_DIRECTORY_HINTS}
+        ${EIGEN3_DIRECTORY_HINTS}
         ${HDF5_DIRECTORY_HINTS}
         )
 
 if(H5PP_PREFER_CONDA_LIBS)
-    list(APPEND CMAKE_PREFIX_PATH
+    list(APPEND CONDA_CANDIDATE_PATHS
             $ENV{CONDA_PREFIX}
             $ENV{HOME}/anaconda3
             $ENV{HOME}/anaconda
@@ -17,7 +22,14 @@ if(H5PP_PREFER_CONDA_LIBS)
             $ENV{HOME}/miniconda
             $ENV{HOME}/.conda
             )
+    foreach(path ${CONDA_CANDIDATE_PATHS})
+        if(EXISTS path)
+            list(APPEND CMAKE_PREFIX_PATH ${path})
+        endif()
+    endforeach()
+    unset(CONDA_CANDIDATE_PATHS)
 endif()
+
 list(APPEND CMAKE_PREFIX_PATH
         $ENV{EBROOTHDF5}
         $ENV{EBROOTSPDLOG}
@@ -25,3 +37,5 @@ list(APPEND CMAKE_PREFIX_PATH
         ${HDF5_ROOT}
         $ENV{HDF5_ROOT}
         )
+
+list(REMOVE_DUPLICATES CMAKE_PREFIX_PATH)
