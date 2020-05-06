dee97f74066189140bc2835d00e99ab0521f7000
DavidAce <aceituno@kth.se>
2020-05-06 17:56:56 +0200

    Updated zlib search for hdf5 builds

--- cmake/BuildDependency.cmake
+++ cmake/BuildDependency.cmake
@@ -5,6 +5,7 @@ function(build_dependency dep_name install_dir extra_flags)
         mark_as_advanced(install_dir)
     endif ()
 
+    execute_process( COMMAND  ${CMAKE_COMMAND} -E remove ${build_dir}/CMakeCache.txt)
     execute_process( COMMAND  ${CMAKE_COMMAND} -E make_directory ${build_dir})
     execute_process(
             COMMAND  ${CMAKE_COMMAND}
@@ -17,22 +18,14 @@ function(build_dependency dep_name install_dir extra_flags)
             ${PROJECT_SOURCE_DIR}/cmake/external_${dep_name}
             WORKING_DIRECTORY ${build_dir}
             RESULT_VARIABLE config_result
-            ERROR_VARIABLE  config_error
-
     )
-    if(${config_result})
+    if(config_result)
         message(STATUS "Got non-zero exit code while configuring ${dep_name}")
         message(STATUS  "build_dir         : ${build_dir}")
         message(STATUS  "install_dir       : ${install_dir}")
         message(STATUS  "extra_flags       : ${extra_flags}")
         message(STATUS  "config_result     : ${config_result}")
-        message(STATUS  "Output saved to ${build_dir}/stdout and ${build_dir}/stderr")
-        file(APPEND ${build_dir}/stdout ${config_result})
-        file(APPEND ${build_dir}/stderr ${config_error})
-        if(CMAKE_VERBOSE_MAKEFILE)
-            message(STATUS "Contents of stdout: \n  ${config_result} \n")
-            message(STATUS "Contents of stderr: \n  ${config_error}  \n")
-        endif()
+        message(FATAL_ERROR "Failed to configure ${dep_name}")
     endif()
 
 
@@ -50,13 +43,7 @@ function(build_dependency dep_name install_dir extra_flags)
         message(STATUS  "install_dir       : ${install_dir}")
         message(STATUS  "extra_flags       : ${extra_flags}")
         message(STATUS  "build_result      : ${build_result}")
-        message(STATUS  "Output saved to ${build_dir}/stdout and ${build_dir}/stderr")
-        file(APPEND ${build_dir}/stdout ${build_result})
-        file(APPEND ${build_dir}/stderr ${build_error})
-        if(CMAKE_VERBOSE_MAKEFILE)
-            message(STATUS "Contents of stdout: \n  ${build_result} \n")
-            message(STATUS "Contents of stderr: \n  ${build_error} \n")
-        endif()
+        message(FATAL_ERROR "Failed to build ${dep_name}")
     endif()
 
     # Copy the install manifest if it exists
