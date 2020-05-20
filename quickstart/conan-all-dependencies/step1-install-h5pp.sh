4a3812a94f6fc2f9d4ffa618dd692033de6f5d88
DavidAce <aceituno@kth.se>
2020-05-20 13:56:30 +0200

    Updated conan version

--- quickstart/conan-all-dependencies/step1-install-h5pp.sh
+++ quickstart/conan-all-dependencies/step1-install-h5pp.sh
@@ -4,4 +4,4 @@
 conan remote add conan-h5pp https://api.bintray.com/conan/davidace/conan-public
 
 # Install h5pp. Use your own profile
-conan install h5pp/1.7.0@davidace/stable --profile default
+conan install h5pp/1.7.2@davidace/stable --profile default
