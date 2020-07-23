4b4663a60add25495fc2e1a742075008787d8f72
DavidAce <aceituno@kth.se>
2020-07-23 15:22:57 +0200

    Include iostream which was removed in h5pp headers

--- examples/example-step1-text.cpp
+++ examples/example-step1-text.cpp
@@ -1,28 +1,26 @@
 #include <h5pp/h5pp.h>
+#include <iostream>
 
-//In this example weuse h5pp to write and read an std::string
+// In this example weuse h5pp to write and read an std::string
 // By default, h5pp stores std::string data as a variable-length array, i.e. H5T_VARIABLE.
 // For fixed-size datasets use const char * or specify the size of the std::string buffer.
 // By default, UTF-8 encoding is used.
-
-
 
 int main() {
     // Initialize a file
     h5pp::File file("exampledir/example-step1-text.h5", h5pp::FilePermission::REPLACE);
 
     std::string writeText = "Hello world";
-    file.writeDataset(writeText,"stringData"); // Write data to file in dataset named "stringData"
+    file.writeDataset(writeText, "stringData"); // Write data to file in dataset named "stringData"
 
     std::string readText;
-    file.readDataset(readText,"stringData"); // Read data. The string readText is resized appropriately by h5pp
+    file.readDataset(readText, "stringData"); // Read data. The string readText is resized appropriately by h5pp
 
     // Alternatively, you can read by assignment
     auto readText_alt = file.readDataset<std::string>("stringData");
 
-
     printf("Wrote dataset: %s\n", writeText.c_str());
-    printf("Read  dataset: %s | alt: %s \n", readText.c_str(),readText_alt.c_str());
+    printf("Read  dataset: %s | alt: %s \n", readText.c_str(), readText_alt.c_str());
 
     return 0;
 }
--- examples/example-step2-stdvector-text.cpp
+++ examples/example-step2-stdvector-text.cpp
@@ -1,42 +1,39 @@
 #include <h5pp/h5pp.h>
-
+#include <iostream>
 // In this example we write text-data to a file in a
 // In h5pp, each string is treated as a single "scalar" unit of variable-length
 // Hence, size corresponds to the number of strings, not the length of a string.
 
 int main() {
     // Initialize a file
-    h5pp::File file("exampledir/example-step2-text.h5", h5pp::FilePermission::REPLACE,0);
+    h5pp::File  file("exampledir/example-step2-text.h5", h5pp::FilePermission::REPLACE, 0);
     std::string singleString = "This is a dummy string";
 
     // Write to file
-    file.writeDataset(singleString,"example-group/mySingleString");
+    file.writeDataset(singleString, "example-group/mySingleString");
 
-    //Read from file
+    // Read from file
     auto singleStringRead = file.readDataset<std::string>("example-group/mySingleString");
 
     // Print it
     std::cout << "h5pp wrote: " << singleString << std::endl;
     std::cout << "h5pp read : " << singleStringRead << std::endl;
 
-
     // We can write multiple strings at a time ...
     std::vector<std::string> multipleStrings;
     multipleStrings.emplace_back("this is a vector");
     multipleStrings.emplace_back("of strings");
     multipleStrings.emplace_back("of varying lengths");
-    file.writeDataset(multipleStrings,"example-group/multipleStrings");
+    file.writeDataset(multipleStrings, "example-group/multipleStrings");
 
     // and read them back in one go...
     auto multipleStringsRead = file.readDataset<std::vector<std::string>>("example-group/multipleStrings");
     std::cout << "Multiple strings as vector\n";
-    for(auto & str : multipleStringsRead) std::cout << str << std::endl;
+    for(auto &str : multipleStringsRead) std::cout << str << std::endl;
 
     // or read them all into a single string! We use \n as separator for each element.
     auto multipleStringsAsString = file.readDataset<std::string>("example-group/multipleStrings");
-    std::cout << "Multiple strings as string: \n"<< multipleStringsAsString << std::endl;
-
-
+    std::cout << "Multiple strings as string: \n" << multipleStringsAsString << std::endl;
 
     return 0;
 }
