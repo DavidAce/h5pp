ae19216a923b4e9f7657a656f438c50bc13fda27
DavidAce <aceituno@kth.se>
2020-07-23 16:33:11 +0200

    Work in progress

--- examples/example-step3-eigen.cpp
+++ examples/example-step3-eigen.cpp
@@ -1,10 +1,10 @@
-#if __has_include(<Eigen/Core>)
 #include <h5pp/h5pp.h>
 #include <iostream>
 int main() {
     // Initialize a file
-    h5pp::File file("exampledir/example-step3-eigen.h5", h5pp::FilePermission::REPLACE,0);
+    h5pp::File file("exampledir/example-step3-eigen.h5", h5pp::FilePermission::REPLACE, 0);
 
+#ifdef H5PP_EIGEN3
     // Initialize a 10x10 Eigen matrix with random complex entries
     Eigen::MatrixXcd m1 = Eigen::MatrixXcd::Random(5, 5);
 
@@ -16,5 +16,6 @@ int main() {
     auto m2 = file.readDataset<Eigen::MatrixXcd>("myGroup/myEigenMatrix");
 
     std::cout << m2 << std::endl;
-    return 0;}
 #endif
+    return 0;
+}
