809f7325b5d338a916a906a08d459776de7be14a
DavidAce <aceituno@kth.se>
2020-08-03 10:13:03 +0200

    Remove old test code

--- tests/test-copyFile.cpp
+++ tests/test-copyFile.cpp
@@ -1,32 +1,12 @@
-#include <complex>
 #include <h5pp/h5pp.h>
-#include <iostream>
-
-/*! \brief Prints the content of a vector nicely */
-template<typename T> std::ostream &operator<<(std::ostream &out, const std::vector<T> &v) {
-    if(!v.empty()) {
-        out << "[ ";
-        std::copy(v.begin(), v.end(), std::ostream_iterator<T>(out, " "));
-        out << "]";
-    }
-    return out;
-}
-
-using namespace std::complex_literals;
-
-// Store some dummy data to an hdf5 file
 
 int main() {
-    static_assert(h5pp::type::sfinae::has_data<std::vector<double>>() and
-                  "Compile time type-checker failed. Could not properly detect class member data. Check that you are using a supported compiler!");
-
     size_t     logLevel = 2;
     h5pp::File fileA("output/copyFileA.h5", h5pp::FilePermission::REPLACE, logLevel);
     h5pp::File fileB("output/copyFileB.h5", h5pp::FilePermission::REPLACE, logLevel);
 
     fileA.writeDataset("A", "groupA/A");
     fileB.writeDataset("B", "groupB/B");
-
 
 
     // Try copying the file
