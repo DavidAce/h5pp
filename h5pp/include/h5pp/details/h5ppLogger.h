31cce799843dd37a9e9b7511c6558ff944a50ca1
DavidAce <aceituno@kth.se>
2020-08-21 09:16:35 +0200

    Remove old debug logging

--- h5pp/include/h5pp/details/h5ppLogger.h
+++ h5pp/include/h5pp/details/h5ppLogger.h
@@ -39,7 +39,7 @@ namespace h5pp::logger {
             throw std::runtime_error("Given wrong type for spdlog verbosity level");
         }
         //        log->info("Log verbosity level: {}   | trace:0 | debug:1 | info:2 | warn:3 | error:4 | critical:5 |", static_cast<int>(log->level()));
-        log->debug("Log verbosity level: {}", static_cast<int>(log->level()));
+        //        log->debug("Log verbosity level: {}", static_cast<int>(log->level()));
     }
 
     inline void setLogger(const std::string &name, std::optional<size_t> levelZeroToFive = std::nullopt, std::optional<bool> timestamp = std::nullopt) {
