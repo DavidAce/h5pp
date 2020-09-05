98eb37367e64605d0136a8a92c7cb8c131a421e6
DavidAce <aceituno@kth.se>
2020-09-05 12:13:17 +0200

    Moved fmt includes into h5ppFormat where they belong

--- h5pp/include/h5pp/details/h5ppFormat.h
+++ h5pp/include/h5pp/details/h5ppFormat.h
@@ -1,16 +1,56 @@
 #pragma once
 
-#include "h5ppSpdlog.h"
+
+#if defined (SPDLOG_FMT_EXTERNAL) && __has_include(<fmt/core.h>) &&  __has_include(<fmt/format.h>) && __has_include(<fmt/ranges.h>) &&  __has_include(<fmt/ostream.h>)
+#include <fmt/core.h>
+    #include <fmt/format.h>
+    #include <fmt/ostream.h>
+    #include <fmt/ranges.h>
+#elif !defined(SPDLOG_FMT_EXTERNAL) && __has_include(<spdlog/fmt/fmt.h>) && __has_include(<spdlog/fmt/bundled/ranges.h>) &&  __has_include(<spdlog/fmt/bundled/ostream.h>)
+#include <spdlog/fmt/bundled/ostream.h>
+#include <spdlog/fmt/bundled/ranges.h>
+#include <spdlog/fmt/fmt.h>
+#elif __has_include(<fmt/core.h>) &&  __has_include(<fmt/format.h>) && __has_include(<fmt/ranges.h>) &&  __has_include(<fmt/ostream.h>)
+// Check if there are already fmt headers installed independently from Spdlog
+    // Note that in this case the user hasn't enabled Spdlog for h5pp, so the build hasn't linked any compiled FMT libraries
+    // To avoid undefined references we coult opt in to the header-only mode of FMT.
+    // Note that this check should be skipped if using conan. Then, SPDLOG_FMT_EXTERNAL is defined
+    #ifdef FMT_HEADER_ONLY
+        #pragma message "{fmt} has been included as header-only library by defining the compile option FMT_HEADER_ONLY. This may cause a large compile-time overhead"
+    #endif
+    #include <fmt/core.h>
+    #include <fmt/format.h>
+    #include <fmt/ostream.h>
+    #include <fmt/ranges.h>
+#else
+    // In this case there is no fmt so we make our own simple formatter
+    #pragma message("h5pp warning: could not find header fmt library headers <fmt/core.h> or <spdlog/fmt/fmt.h>: A hand-made formatter will be used instead. Consider using the fmt library for maximum performance")
+
+#endif
+
 
 namespace h5pp {
 
 #if defined(FMT_FORMAT_H_)
+
+namespace h5pp {
     template<typename... Args>
     [[nodiscard]] std::string format(Args... args) {
         return fmt::format(std::forward<Args>(args)...);
     }
 
 #else
+
+    // In this case there is no fmt so we make our own simple formatter
+    #include "h5ppTypeSfinae.h"
+    #include <algorithm>
+    #include <iostream>
+    #include <list>
+    #include <memory>
+    #include <sstream>
+    #include <string>
+
+namespace h5pp {
     namespace formatting {
 
         template<class T, class... Ts>
--- h5pp/include/h5pp/details/h5ppSpdlog.h
+++ h5pp/include/h5pp/details/h5ppSpdlog.h
@@ -1,37 +1,6 @@
 #pragma once
 
-#if defined (SPDLOG_FMT_EXTERNAL) && __has_include(<fmt/core.h>) &&  __has_include(<fmt/format.h>) && __has_include(<fmt/ranges.h>) &&  __has_include(<fmt/ostream.h>)
-    #include <fmt/core.h>
-    #include <fmt/format.h>
-    #include <fmt/ostream.h>
-    #include <fmt/ranges.h>
-#elif !defined(SPDLOG_FMT_EXTERNAL) && __has_include(<spdlog/fmt/fmt.h>) && __has_include(<spdlog/fmt/bundled/ranges.h>) &&  __has_include(<spdlog/fmt/bundled/ostream.h>)
-    #include <spdlog/fmt/bundled/ostream.h>
-    #include <spdlog/fmt/bundled/ranges.h>
-    #include <spdlog/fmt/fmt.h>
-#elif __has_include(<fmt/core.h>) &&  __has_include(<fmt/format.h>) && __has_include(<fmt/ranges.h>) &&  __has_include(<fmt/ostream.h>)
-    // Check if there are already fmt headers installed independently from Spdlog
-    // Note that in this case the user hasn't enabled Spdlog for h5pp, so the build hasn't linked any compiled FMT libraries
-    // To avoid undefined references we coult opt in to the header-only mode of FMT.
-    // Note that this check should be skipped if using conan. Then, SPDLOG_FMT_EXTERNAL is defined
-    #ifdef FMT_HEADER_ONLY
-        #pragma message "{fmt} has been included as header-only library by defining the compile option FMT_HEADER_ONLY. This may cause a large compile-time overhead"
-    #endif
-    #include <fmt/core.h>
-    #include <fmt/format.h>
-    #include <fmt/ostream.h>
-    #include <fmt/ranges.h>
-#else
-// In this case there is no fmt so we make our own simple formatter
-    #include "h5ppTypeSfinae.h"
-    #include <algorithm>
-    #include <iostream>
-    #include <list>
-    #include <memory>
-    #include <sstream>
-    #include <string>
-#endif
-
+#include "h5ppFormat.h"
 
 
 #if __has_include(<spdlog/spdlog.h>) && __has_include(<spdlog/sinks/stdout_color_sinks.h>)
