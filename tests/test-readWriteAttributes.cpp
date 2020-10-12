a442cba1d022f37747b44d53cbb8abe93794bd2b
DavidAce <aceituno@kth.se>
2020-10-12 11:13:09 +0200

    Removed unnecessary compile time check

--- tests/test-readWriteAttributes.cpp
+++ tests/test-readWriteAttributes.cpp
@@ -13,8 +13,6 @@ template<typename T> std::ostream &operator<<(std::ostream &out, const std::vect
 }
 
 int main() {
-    static_assert(h5pp::type::sfinae::has_data<std::vector<double>>() and "Compile time type-checker failed. Could not properly detect class member data. Check that you are "
-                                                                          "using a supported compiler!");
     // Generate dummy data
     int                               AttributeInt                 = 7;
     double                            AttributeDouble              = 47.4;
