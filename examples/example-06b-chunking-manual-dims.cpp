73b83d456676ff6e1d71d6541e37edfb1117caaf
DavidAce <aceituno@kth.se>
2020-10-27 07:47:15 +0100

    Fixed typos in example comments

--- examples/example-06b-chunking-manual-dims.cpp
+++ examples/example-06b-chunking-manual-dims.cpp
@@ -25,25 +25,25 @@ int main() {
 
     /*
      * Note 1: h5pp deduces "good" chunking dimensions automatically when none are given.
-     *         Because h5pp doesn't know the future size of a dataset,
-     *         it will guess that each chunk is an n-cube of ~10 to 1000 KB.
-     *         depending on the initial size and type.
+     *         Since h5pp doesn't know the future size of a dataset,
+     *         it will guess assume that each chunk is an n-cube of size 10 KB to 1000 KB,
+     *         depending on the initial size and type of the given container.
      *         Chunk dimensions affect IO performance and compression efficiency,
      *         so setting chunking dimensions automatically may be OK but not
      *         necessarily optimal for performance.
      *
-     * Note 2: Here we set the chunk dimensions manually. It is not always obvious what
+     * Note 2: In this example we set the chunk dimensions manually. It is not always obvious what
      *         good chunking dimensions are: best is to benchmark your particular case.
-     *         A good rule of thumb is to aim for ~10 KB to 1000 KB worth of elements in suitable
-     *         shape. For 1-dimensional arrays there is only one option.
+     *         A good rule of thumb is to aim for 10 KB to 1000 KB worth of elements in suitable
+     *         shape. For 1-dimensional arrays there is only one option for the dimensions.
      *         In this example we choose chunks containing 10000 elements, so each
      *         chunk becomes 80 KB (because sizeof(double) * 10K = 80KB).
      *
-     * Note 3: The dataset size argument {1000} is optional and can be skipped using "std::nullopt".
-     *         In fact, arguments taken as type "h5pp::OptDimsType" has this property.
+     * Note 3: The dataset size argument "{1000}" is optional and can be skipped using "std::nullopt".
+     *         In fact, all arguments taken as type "h5pp::OptDimsType" have this property.
      *         If h5pp detects no argument or if std::nullopt is given, h5pp will deduce the shape
      *         based on the given data container. If you pass a C-style pointer to a raw data buffer
-     *         the shapes can't be deduced so you must pass dimensions explicitly.
+     *         then shapes can't be deduced so you must pass dimensions explicitly.
      */
 
     // Write data
