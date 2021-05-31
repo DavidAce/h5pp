870c5c55a5221df87b307c7be029f60a2a131b46
DavidAce <aceituno@kth.se>
2021-05-31 18:44:01 +0200

    New H5PP_PACKAGE_MANAGER option: fetch
    * Uses FetchContent mechanism to add dependencies
    * Note that HDF5 v1.12 does not support add_subdirectory yet. Therefore it has to be built with execute_process + externalproject_add as before.

---
+++ cmake/fetch/FetchEigen3.cmake
@@ -0,0 +1,18 @@
+cmake_minimum_required(VERSION 3.14)
+include(FetchContent)
+FetchContent_Declare(fetch-eigen3
+        URL      https://gitlab.com/libeigen/eigen/-/archive/3.4-rc1/eigen-3.4-rc1.tar.gz
+        URL_MD5  0839b9721e65d2328fb96eb4290d74cc
+        QUIET
+        CMAKE_ARGS
+        -DEIGEN_TEST_CXX11:BOOL=ON
+        -DBUILD_TESTING:BOOL=OFF
+        -DEIGEN_BUILD_DOC:BOOL=OFF
+        -DEIGEN_BUILD_PKGCONFIG:BOOL=OFF
+        )
+if(NOT TARGET Eigen3::Eigen)
+    FetchContent_MakeAvailable(fetch-eigen3) # Avoid needless configure on header only library
+    add_library(Eigen3::Eigen ALIAS eigen)
+endif()
+
+
