afdcce7cf02b630b75dba7e03fa828904cc55864
DavidAce <aceituno@kth.se>
2021-06-01 13:19:33 +0200

    Refactor the setup of std::filesystem to avoid code duplication

--- cmake/SetupDependenciesFind.cmake
+++ cmake/SetupDependenciesFind.cmake
@@ -1,28 +1,3 @@
-cmake_minimum_required(VERSION 3.14)
-
-# h5pp requires the filesystem header (and possibly stdc++fs library)
-find_package(Filesystem COMPONENTS Final Experimental)
-if (TARGET std::filesystem)
-    target_link_libraries(deps INTERFACE std::filesystem)
-    list(APPEND H5PP_TARGETS std::filesystem)
-elseif (H5PP_PACKAGE_MANAGER MATCHES "cmake|conan")
-    message(STATUS "Your compiler lacks std::filesystem. A drop-in replacement ghc::filesystem will be downloaded")
-    message(STATUS "Read more about ghc::filesystem here: https://github.com/gulrak/filesystem")
-    include(cmake/Get_ghcFilesystem.cmake)
-    if(TARGET ghcFilesystem::ghc_filesystem)
-        add_library(std::filesystem INTERFACE IMPORTED)
-        target_link_libraries(std::filesystem INTERFACE ghcFilesystem::ghc_filesystem)
-        target_link_libraries(deps INTERFACE std::filesystem)
-        list(APPEND H5PP_TARGETS std::filesystem)
-    endif()
-else()
-    message(STATUS "Your compiler lacks std::filesystem. Set H5PP_PACKAGE_MANAGER to 'find-or-cmake', 'cmake' or 'conan' or to get the ghc::filesystem replacement")
-    message(STATUS "Read more about ghc::filesystem here: https://github.com/gulrak/filesystem")
-    message(FATAL_ERROR "<filesystem> header and/or library not found")
-endif()
-
-
-
 
 if(H5PP_PACKAGE_MANAGER MATCHES "find")
     if(NOT H5PP_PACKAGE_MANAGER MATCHES "cmake")
