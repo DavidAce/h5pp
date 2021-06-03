5230af5cdb7ba54ac852e4df7b01e9054d33cc28
DavidAce <aceituno@kth.se>
2021-06-03 12:58:02 +0200

    Update PrintTargetProperties.cmake to support LOCATION on INTERFACE IMPORTED libraries

--- cmake/PrintTargetProperties.cmake
+++ cmake/PrintTargetProperties.cmake
@@ -10,7 +10,7 @@ execute_process(COMMAND cmake --help-property-list OUTPUT_VARIABLE CMAKE_PROPERT
 STRING(REGEX REPLACE ";" "\\\\;" CMAKE_PROPERTY_LIST "${CMAKE_PROPERTY_LIST}")
 STRING(REGEX REPLACE "\n" ";" CMAKE_PROPERTY_LIST "${CMAKE_PROPERTY_LIST}")
 # Fix https://stackoverflow.com/questions/32197663/how-can-i-remove-the-the-location-property-may-not-be-read-from-target-error-i
-list(FILTER CMAKE_PROPERTY_LIST EXCLUDE REGEX "^LOCATION$|^LOCATION_|_LOCATION$")
+#list(FILTER CMAKE_PROPERTY_LIST EXCLUDE REGEX "^LOCATION$|^LOCATION_|_LOCATION$")
 # For some reason, "TYPE" shows up twice - others might too?
 list(REMOVE_DUPLICATES CMAKE_PROPERTY_LIST)
 
@@ -19,7 +19,8 @@ list(REMOVE_DUPLICATES CMAKE_PROPERTY_LIST)
 # doesn't exist!
 unset(CMAKE_WHITELISTED_PROPERTY_LIST)
 foreach(prop ${CMAKE_PROPERTY_LIST})
-    if(prop MATCHES "^(INTERFACE|[_a-z]|IMPORTED_LIBNAME_|MAP_IMPORTED_CONFIG_)|^(COMPATIBLE_INTERFACE_(BOOL|NUMBER_MAX|NUMBER_MIN|STRING)|EXPORT_NAME|IMPORTED(_GLOBAL|_CONFIGURATIONS|_LIBNAME)?|NAME|TYPE|NO_SYSTEM_FROM_IMPORTED)$")
+    # Edit: Added location
+    if(prop MATCHES "(^LOCATION$|^LOCATION_|_LOCATION$)|^(INTERFACE|[_a-z]|IMPORTED_LIBNAME_|MAP_IMPORTED_CONFIG_)|^(COMPATIBLE_INTERFACE_(BOOL|NUMBER_MAX|NUMBER_MIN|STRING)|EXPORT_NAME|IMPORTED(_GLOBAL|_CONFIGURATIONS|_LIBNAME)?|NAME|TYPE|NO_SYSTEM_FROM_IMPORTED)$")
         list(APPEND CMAKE_WHITELISTED_PROPERTY_LIST ${prop})
     endif()
 endforeach()
@@ -39,15 +40,21 @@ function(print_target_properties tgt)
     endif()
 
     get_target_property(target_type ${tgt} TYPE)
-    if(target_type MATCHES "INTERFACE_LIBRARY" OR target_type MATCHES "EXECUTABLE")
+    get_target_property(target_imported ${tgt} IMPORTED)
+
+    if(NOT target_imported OR target_type MATCHES "EXECUTABLE")
+        set(PROP_LIST ${CMAKE_WHITELISTED_PROPERTY_LIST})
+        list(FILTER PROP_LIST EXCLUDE REGEX "^LOCATION$|^LOCATION_|_LOCATION$")
+    elseif(target_imported AND target_type MATCHES "INTERFACE_LIBRARY") # An "INTERFACE IMPORTED" library
         set(PROP_LIST ${CMAKE_WHITELISTED_PROPERTY_LIST})
     else()
         set(PROP_LIST ${CMAKE_PROPERTY_LIST})
     endif()
+    string(TOUPPER ${CMAKE_BUILD_TYPE} CMAKE_BUILD_TYPE_UPPER )
 
     foreach (prop ${PROP_LIST})
-        string(REPLACE "<CONFIG>" "${CMAKE_BUILD_TYPE}" prop ${prop})
-        # message ("Checking ${prop}")
+        string(REPLACE "<CONFIG>" "${CMAKE_BUILD_TYPE_UPPER}" prop ${prop})
+        string(REPLACE "<LANG>" "CXX" prop ${prop})
         get_property(propval TARGET ${tgt} PROPERTY ${prop} SET)
         if (propval)
             get_target_property(propval ${tgt} ${prop})
