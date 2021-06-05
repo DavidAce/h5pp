36871a76d2de1c9fac11a5049e6a6dc2c842867f
DavidAce <aceituno@kth.se>
2021-06-05 09:54:34 +0200

    More complete CMake configure message

--- cmake/PrintBuildInfo.cmake
+++ cmake/PrintBuildInfo.cmake
@@ -1,8 +1,5 @@
 cmake_minimum_required(VERSION 3.19)
 if(H5PP_PRINT_INFO)
-
-    message(STATUS "CMake Version ${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION}.${CMAKE_PATCH_VERSION}")
-
     # Print host properties
     cmake_host_system_information(RESULT _host_name QUERY HOSTNAME)
     cmake_host_system_information(RESULT _proc_type QUERY PROCESSOR_DESCRIPTION)
@@ -18,20 +15,27 @@ if(H5PP_PRINT_INFO)
             "-- | ${_proc_type}\n"
             "-- | ${_os_version}")
 
-    # Print CMake options
+    # Print all CMake options
     message(STATUS  "|----------------\n"
-            "-- | BUILD_SHARED_LIBS       : ${BUILD_SHARED_LIBS}\n"
-            "-- | H5PP_BUILD_EXAMPLES     : ${H5PP_BUILD_EXAMPLES}\n"
-            "-- | H5PP_ENABLE_TESTS       : ${H5PP_ENABLE_TESTS}\n"
-            "-- | H5PP_ENABLE_EIGEN3      : ${H5PP_ENABLE_EIGEN3}\n"
-            "-- | H5PP_ENABLE_SPDLOG      : ${H5PP_ENABLE_SPDLOG}\n"
-            "-- | H5PP_ENABLE_FMT         : ${H5PP_ENABLE_FMT}\n"
-            "-- | H5PP_ENABLE_MPI         : ${H5PP_ENABLE_MPI}\n"
-            "-- | H5PP_IS_SUBPROJECT      : ${H5PP_IS_SUBPROJECT}\n"
-            "-- | H5PP_PACKAGE_MANAGER    : ${H5PP_PACKAGE_MANAGER}\n"
-            "-- | H5PP_DEPS_IN_SUBDIR     : ${H5PP_DEPS_IN_SUBDIR}\n"
-            "-- | H5PP_PREFER_CONDA_LIBS  : ${H5PP_PREFER_CONDA_LIBS}\n"
-            "-- | H5PP_PRINT_INFO         : ${H5PP_PRINT_INFO}\n"
-            "-- | CMAKE_PREFIX_PATH       : ${CMAKE_PREFIX_PATH}\n"
-            "-- | CMAKE_INSTALL_PREFIX    : ${CMAKE_INSTALL_PREFIX}\n")
+            "-- | CMAKE_BUILD_TYPE         : ${CMAKE_BUILD_TYPE}\n"
+            "-- | CMAKE_PREFIX_PATH        : ${CMAKE_PREFIX_PATH}\n"
+            "-- | CMAKE_INSTALL_PREFIX     : ${CMAKE_INSTALL_PREFIX}\n"
+            "-- | BUILD_SHARED_LIBS        : ${BUILD_SHARED_LIBS}\n"
+            "-- | H5PP_BUILD_EXAMPLES      : ${H5PP_BUILD_EXAMPLES}\n"
+            "-- | H5PP_ENABLE_TESTS        : ${H5PP_ENABLE_TESTS}\n"
+            "-- | H5PP_ENABLE_EIGEN3       : ${H5PP_ENABLE_EIGEN3}\n"
+            "-- | H5PP_ENABLE_SPDLOG       : ${H5PP_ENABLE_SPDLOG}\n"
+            "-- | H5PP_ENABLE_FMT          : ${H5PP_ENABLE_FMT}\n"
+            "-- | H5PP_ENABLE_MPI          : ${H5PP_ENABLE_MPI}\n"
+            "-- | H5PP_IS_SUBPROJECT       : ${H5PP_IS_SUBPROJECT}\n"
+            "-- | H5PP_PACKAGE_MANAGER     : ${H5PP_PACKAGE_MANAGER}\n"
+            "-- | H5PP_PREFIX_ADD_PKGNAME  : ${H5PP_PREFIX_ADD_PKGNAME}\n"
+            "-- | H5PP_PREFER_CONDA_LIBS   : ${H5PP_PREFER_CONDA_LIBS}\n"
+            "-- | H5PP_PRINT_INFO          : ${H5PP_PRINT_INFO}\n")
+else()
+    # Print less CMake options
+    message(STATUS "CMake Version ${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION}.${CMAKE_PATCH_VERSION}")
+    message(STATUS  "|----------------\n"
+            "-- | CMAKE_BUILD_TYPE         : ${CMAKE_BUILD_TYPE}\n"
+            "-- | CMAKE_INSTALL_PREFIX     : ${CMAKE_INSTALL_PREFIX}\n")
 endif ()
