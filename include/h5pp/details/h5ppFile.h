cc916b136f3d0e6841a276564a7b5379ebc26561
DavidAce <aceituno@kth.se>
2021-08-29 19:10:57 +0200

    Revert solution with H5F_CLOSE_WEAK and opt for keeping the file handle alive manually when needed.
    * Other problems arise when a file is reopened with different close degrees. Better to keep a consistent close degree.

--- include/h5pp/details/h5ppFile.h
+++ include/h5pp/details/h5ppFile.h
@@ -97,17 +97,15 @@ namespace h5pp {
          * - The file permission is set when initializing h5pp::File.
          * - Use `h5pp::setKeepFileOpened()` to keep a cached handle. Use `h5pp::setKeepFileClosed()` to close the cached handle.
          */
-        [[nodiscard]] hid::h5f openFileHandle(std::optional<H5F_close_degree_t> closeDegree = std::nullopt) const {
+        [[nodiscard]] hid::h5f openFileHandle() const {
             h5pp::logger::setLogger("h5pp|" + filePath.filename().string(), logLevel, logTimestamp);
             if(fileHandle) return fileHandle.value();
             // Give the option to override the close degree
-            // When a file handle is closed, the default in h5pp is to first close all associated id's, and then close the file (H5F_CLOSE_STRONG)
-            // Setting H5F_CLOSE_WEAK keeps the file handle alive until associated id's are closed.
-            hid::h5p fileAccess = plists.fileAccess;
-            if(closeDegree) H5Pset_fclose_degree(fileAccess, closeDegree.value());
+            // When a file handle is closed, the default in h5pp is to first close all associated id's, and then close the file
+            // (H5F_CLOSE_STRONG) Setting H5F_CLOSE_WEAK keeps the file handle alive until associated id's are closed.
             if(permission == h5pp::FilePermission::READONLY) {
                 h5pp::logger::log->trace("Opening file in READONLY mode");
-                hid_t fid = H5Fopen(filePath.string().c_str(), H5F_ACC_RDONLY, fileAccess);
+                hid_t fid = H5Fopen(filePath.string().c_str(), H5F_ACC_RDONLY, plists.fileAccess);
                 if(fid < 0) {
                     H5Eprint(H5E_DEFAULT, stderr);
                     throw std::runtime_error(h5pp::format("Failed to open file in read-only mode [{}]", filePath.string()));
@@ -115,7 +113,7 @@ namespace h5pp {
                     return fid;
             } else {
                 h5pp::logger::log->trace("Opening file in READWRITE mode");
-                hid_t fid = H5Fopen(filePath.string().c_str(), H5F_ACC_RDWR, fileAccess);
+                hid_t fid = H5Fopen(filePath.string().c_str(), H5F_ACC_RDWR, plists.fileAccess);
                 if(fid < 0) {
                     H5Eprint(H5E_DEFAULT, stderr);
                     throw std::runtime_error(h5pp::format("Failed to open file in read-write mode [{}]", filePath.string()));
@@ -425,20 +423,18 @@ namespace h5pp {
          * Example 2: If compressionLevel == std::nullopt, the current compression level is returned.
          */
         [[nodiscard]] unsigned int getCompressionLevel(std::optional<unsigned int> compressionLevel /*!< Suggested compression level */
-                                                       ) const {
+        ) const {
             if(compressionLevel)
                 return h5pp::hdf5::getValidCompressionLevel(compressionLevel.value());
             else
                 return currentCompressionLevel;
         }
 
-
         /*
          *
          * Functions related to groups and datasets
          *
          */
-
 
         void createGroup(std::string_view group_relative_name) {
             h5pp::hdf5::createGroup(openFileHandle(), group_relative_name, std::nullopt, plists);
@@ -1385,21 +1381,25 @@ namespace h5pp {
          */
 
         [[nodiscard]] int getDatasetRank(std::string_view datasetPath) const {
-            auto dataset = h5pp::hdf5::openLink<hid::h5d>(openFileHandle(H5F_CLOSE_WEAK), datasetPath);
+            auto filehdl = openFileHandle();
+            auto dataset = h5pp::hdf5::openLink<hid::h5d>(filehdl, datasetPath);
             return h5pp::hdf5::getRank(dataset);
         }
 
         [[nodiscard]] std::vector<hsize_t> getDatasetDimensions(std::string_view datasetPath) const {
-            auto dataset = h5pp::hdf5::openLink<hid::h5d>(openFileHandle(H5F_CLOSE_WEAK), datasetPath);
+            auto filehdl = openFileHandle();
+            auto dataset = h5pp::hdf5::openLink<hid::h5d>(filehdl, datasetPath);
             return h5pp::hdf5::getDimensions(dataset);
         }
         [[nodiscard]] std::optional<std::vector<hsize_t>> getDatasetMaxDimensions(std::string_view datasetPath) const {
-            auto dataset = h5pp::hdf5::openLink<hid::h5d>(openFileHandle(H5F_CLOSE_WEAK), datasetPath);
+            auto filehdl = openFileHandle();
+            auto dataset = h5pp::hdf5::openLink<hid::h5d>(filehdl, datasetPath);
             return h5pp::hdf5::getMaxDimensions(dataset);
         }
 
         [[nodiscard]] std::optional<std::vector<hsize_t>> getDatasetChunkDimensions(std::string_view datasetPath) const {
-            auto dataset = h5pp::hdf5::openLink<hid::h5d>(openFileHandle(H5F_CLOSE_WEAK), datasetPath);
+            auto filehdl = openFileHandle();
+            auto dataset = h5pp::hdf5::openLink<hid::h5d>(filehdl, datasetPath);
             return h5pp::hdf5::getChunkDimensions(dataset);
         }
 
