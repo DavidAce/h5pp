ce95e9eea444a0556b343d3b6ca4de383a497d3c
DavidAce <aceituno@kth.se>
2021-11-28 22:40:45 +0100

    Use H5PP_PACKAGE_MANAGER in conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -63,7 +63,7 @@ class h5ppConan(ConanFile):
             self._cmake = CMake(self)
             self._cmake.definitions["H5PP_ENABLE_TESTS"]         = self.options.tests
             self._cmake.definitions["H5PP_BUILD_EXAMPLES"]       = self.options.examples
-            self._cmake.definitions["H5PP_PACKAGE_MANAGER"]      = "find"
+            self._cmake.definitions["H5PP_PACKAGE_MANAGER"]      = "none"
             self._cmake.definitions["H5PP_ENABLE_PCH"]           = self.options.pch
             self._cmake.definitions["H5PP_ENABLE_CCACHE"]        = self.options.ccache
             self._cmake.definitions["H5PP_ENABLE_EIGEN3"]        = True
