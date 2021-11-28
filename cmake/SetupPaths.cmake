44e130fbd73513f747b9a2977ab2565c4e990b9c
DavidAce <aceituno@kth.se>
2021-11-28 21:52:20 +0100

    Moved find_package modules to cmake/modules

--- cmake/SetupPaths.cmake
+++ cmake/SetupPaths.cmake
@@ -1,13 +1,13 @@
 cmake_minimum_required(VERSION 3.15)
 
 # Append search paths for find_package and find_library calls
-list(INSERT CMAKE_MODULE_PATH 0 ${PROJECT_SOURCE_DIR}/cmake)
+list(APPEND CMAKE_MODULE_PATH ${PROJECT_SOURCE_DIR}/cmake/modules)
 
 # Make sure find_library prefers static/shared library depending on BUILD_SHARED_LIBS
 # This is important when finding dependencies such as zlib which provides both shared and static libraries.
 # Note that we do not force this cache variable, so users can override it
 if(BUILD_SHARED_LIBS)
-    # This is order is the default
+    # This order is the default
     set(CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_SHARED_LIBRARY_SUFFIX};${CMAKE_STATIC_LIBRARY_SUFFIX} CACHE STRING "Prefer finding shared libraries")
 else()
     set(CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_STATIC_LIBRARY_SUFFIX};${CMAKE_SHARED_LIBRARY_SUFFIX} CACHE STRING "Prefer finding static libraries")
@@ -21,23 +21,26 @@ if(DEFINED CMAKE_INSTALL_PREFIX AND NOT IS_ABSOLUTE CMAKE_INSTALL_PREFIX)
 endif()
 
 # Setup build and install directories for dependencies
-if(NOT H5PP_DEPS_BUILD_DIR)
-    set(H5PP_DEPS_BUILD_DIR ${CMAKE_BINARY_DIR}/h5pp-deps-build)
-endif()
-if(NOT H5PP_DEPS_INSTALL_DIR)
-    set(H5PP_DEPS_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}) # Install to the same location as h5pp by default
+if(H5PP_PACKAGE_MANAGER MATCHES "cmake|cpm|fetch|conan")
+    if(NOT H5PP_DEPS_BUILD_DIR)
+        set(H5PP_DEPS_BUILD_DIR ${CMAKE_BINARY_DIR}/h5pp-deps-build)
+    endif()
+    if(NOT H5PP_DEPS_INSTALL_DIR)
+        set(H5PP_DEPS_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}) # Install to the same location as h5pp by default
+    endif()
+    set(PKG_INSTALL_DIR_DEFAULT ${H5PP_DEPS_INSTALL_DIR} CACHE STRING "" FORCE )
+    set(PKG_BUILD_DIR_DEFAULT   ${H5PP_DEPS_BUILD_DIR}   CACHE STRING "" FORCE )
+    set(CMAKE_PREFIX_PATH "${CMAKE_PREFIX_PATH};${PKG_INSTALL_DIR_DEFAULT};${CMAKE_INSTALL_PREFIX}")
+    list(REMOVE_DUPLICATES CMAKE_PREFIX_PATH)
+    set(CMAKE_PREFIX_PATH "${CMAKE_PREFIX_PATH}" CACHE INTERNAL "Paths for find_package lookup" FORCE)
+    if(H5PP_PREFIX_ADD_PKGNAME)
+        set(CMAKE_INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX}/h5pp CACHE STRING
+                "The option H5PP_PREFIX_ADD_PKGNAME=ON sets the install directory: <CMAKE_INSTALL_PREFIX>/h5pp" FORCE)
+    endif()
+
 endif()
 
-set(PKG_INSTALL_DIR_DEFAULT ${H5PP_DEPS_INSTALL_DIR} CACHE STRING "" FORCE )
-set(PKG_BUILD_DIR_DEFAULT   ${H5PP_DEPS_BUILD_DIR}   CACHE STRING "" FORCE )
-set(CMAKE_PREFIX_PATH "${CMAKE_PREFIX_PATH};${PKG_INSTALL_DIR_DEFAULT};${CMAKE_INSTALL_PREFIX}")
-list(REMOVE_DUPLICATES CMAKE_PREFIX_PATH)
-set(CMAKE_PREFIX_PATH "${CMAKE_PREFIX_PATH}" CACHE INTERNAL "Paths for find_package lookup" FORCE)
 
-if(H5PP_PREFIX_ADD_PKGNAME)
-    set(CMAKE_INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX}/h5pp CACHE STRING
-            "The option H5PP_PREFIX_ADD_PKGNAME=ON sets the install directory: <CMAKE_INSTALL_PREFIX>/h5pp" FORCE)
-endif()
 
 if(WIN32)
     # On Windows it is standard practice to collect binaries into one directory.
