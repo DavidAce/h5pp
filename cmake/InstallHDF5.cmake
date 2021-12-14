d9a141246d500e4a249f62fc2249f2bf441ce575
DavidAce <aceituno@kth.se>
2021-12-14 15:50:33 +0100

    More work on getting HDF5 to be found more robustly in h5ppConfig.cmake
    * FindHDF5.cmake bundled with h5pp defines target HDF5::HDF5.
    * CMake version >= 3.20 defines target HDF5::HDF5
    * CMake version >= 3.19 Earlier versions of CMake define target hdf5::hdf5_hl
    * hdf5-config.cmake from source installations will define target hdf5_hl-<static|shared>
    * it's a bit of a mess...
    
    * Trying to get HDF5 installed from source to propagate ZLIB and SZIP when using config mode
    * Updated ZLIB and SZIP to versions bundled with hdf5 cmake version
    * Added support in InstallHDF5.cmake for build/install subdirectories

--- cmake/InstallHDF5.cmake
+++ cmake/InstallHDF5.cmake
@@ -5,20 +5,32 @@ function(install_hdf5)
     include(cmake/InstallPackage.cmake)
     if(H5PP_PREFIX_ADD_PKGNAME)
         set(INSTALL_PREFIX_PKGNAME INSTALL_PREFIX_PKGNAME)
+        set(INSTALL_SUBDIR hdf5)
     endif()
     if(BUILD_SHARED_LIBS)
         set(HDF5_LINK_TYPE shared)
     else()
         set(HDF5_LINK_TYPE static)
     endif()
-    if(H5PP_PREFIX_ADD_PKGNAME)
-        set(HDF5_ROOT ${H5PP_DEPS_INSTALL_DIR}/hdf5 CACHE PATH "Default root path for HDF5 installed by h5pp" FORCE)
-    else()
-        set(HDF5_ROOT ${H5PP_DEPS_INSTALL_DIR} CACHE PATH "Default root path for HDF5 installed by h5pp" FORCE)
-    endif()
+
+    string(JOIN / H5PP_HDF5_INSTALL_DIR  ${H5PP_DEPS_INSTALL_DIR} ${INSTALL_SUBDIR})
+    set(HDF5_ROOT   ${H5PP_HDF5_INSTALL_DIR} CACHE PATH "Default root path for HDF5 installed by h5pp" FORCE)
+    set(ZLIB_ROOT   ${H5PP_HDF5_INSTALL_DIR} CACHE PATH "Default root path for ZLIB installed by h5pp" FORCE)
+    set(SZIP_ROOT   ${H5PP_HDF5_INSTALL_DIR} CACHE PATH "Default root path for SZIP installed by h5pp" FORCE)
+    set(LIBAEC_ROOT ${H5PP_HDF5_INSTALL_DIR} CACHE PATH "Default root path for LIBAEC installed by h5pp" FORCE)
 
     mark_as_advanced(HDF5_LINK_TYPE)
     mark_as_advanced(HDF5_ROOT)
+    install_package(zlib
+            COMPONENTS static shared
+            TARGET_HINTS zlib zlib-static zlib-shared libz
+            BUILD_SUBDIR hdf5
+            INSTALL_SUBDIR ${INSTALL_SUBDIR})
+    install_package(libaec
+            COMPONENTS static shared
+            TARGET_HINTS sz aec sz-static aec-static sz-shared aec-shared
+            BUILD_SUBDIR hdf5
+            INSTALL_SUBDIR ${INSTALL_SUBDIR})
 
     install_package(hdf5 VERSION 1.12
             COMPONENTS C HL ${HDF5_LINK_TYPE}
