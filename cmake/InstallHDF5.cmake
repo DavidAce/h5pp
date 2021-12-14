34be2c0dd971097b2cffe246b57da1c3efb10be1
DavidAce <aceituno@kth.se>
2021-12-14 03:48:45 +0100

    More work on getting HDF5 to be found more robustly in h5ppConfig.cmake
    * FindHDF5.cmake bundled with h5pp defines target HDF5::HDF5.
    * CMake version >= 3.20 defines target HDF5::HDF5
    * CMake version >= 3.19 Earlier versions of CMake define target hdf5::hdf5_hl
    * hdf5-config.cmake from source installations will define target hdf5_hl-<static|shared>
    * it's a bit of a mess...
    
    * Trying to get HDF5 installed from source to propagate ZLIB and SZIP when using config mode

--- cmake/InstallHDF5.cmake
+++ cmake/InstallHDF5.cmake
@@ -13,33 +13,13 @@ function(install_hdf5)
     endif()
     if(H5PP_PREFIX_ADD_PKGNAME)
         set(HDF5_ROOT ${H5PP_DEPS_INSTALL_DIR}/hdf5 CACHE PATH "Default root path for HDF5 installed by h5pp" FORCE)
-        set(ZLIB_ROOT ${H5PP_DEPS_INSTALL_DIR}/zlib CACHE PATH "Default root path for ZLIB installed by h5pp" FORCE)
     else()
         set(HDF5_ROOT ${H5PP_DEPS_INSTALL_DIR} CACHE PATH "Default root path for HDF5 installed by h5pp" FORCE)
-        set(ZLIB_ROOT ${H5PP_DEPS_INSTALL_DIR} CACHE PATH "Default root path for ZLIB installed by h5pp" FORCE)
     endif()
 
     mark_as_advanced(HDF5_LINK_TYPE)
     mark_as_advanced(HDF5_ROOT)
-    mark_as_advanced(ZLIB_ROOT)
 
-    # Download HDF5
-    # Note that ZLIB and SZIP are build requirements for HDF5, so we install them here if needed
-    install_package(szip
-            NAMES szip sz
-            FIND_NAME SZIP
-            COMPONENTS static shared
-            PATH_SUFFIXES cmake share/cmake # Fixes bug in CMake 3.20.2 not generating search paths
-            ${INSTALL_PREFIX_PKGNAME}
-            )
-    install_package(zlib
-            MODULE
-            FIND_NAME ZLIB
-            LIBRARY_NAMES
-            z zlib zdll zlib1 zlibstatic # Release names
-            zd zlibd zdlld zlibd1 zlib1d zlibstaticd # Debug names
-            ${INSTALL_PREFIX_PKGNAME})
-    set(HDF5_FIND_DEBUG ON)
     install_package(hdf5 VERSION 1.12
             COMPONENTS C HL ${HDF5_LINK_TYPE}
             FIND_NAME HDF5
@@ -52,6 +32,4 @@ function(install_hdf5)
             -DHDF5_ENABLE_SZIP_SUPPORT:BOOL=ON
             )
     set(PKG_hdf5_TARGET ${PKG_hdf5_TARGET} PARENT_SCOPE)
-    set(PKG_zlib_TARGET ${PKG_zlib_TARGET} PARENT_SCOPE)
-    set(PKG_szip_TARGET ${PKG_szip_TARGET} PARENT_SCOPE)
 endfunction()
