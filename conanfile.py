39aa5f1af23acfb9051239a64a35fcefc6a99fa7
DavidAce <aceituno@kth.se>
2022-05-09 09:25:02 +0200

    Modernize conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -1,37 +1,22 @@
-import os
-from conans import ConanFile, CMake, tools
+from conan.tools.microsoft import is_msvc
+from conans import ConanFile, tools
 from conans.errors import ConanInvalidConfiguration
-from conans.errors import ConanException
-from conans.tools import Version
+import os
 
-class h5ppConan(ConanFile):
+required_conan_version = ">=1.45.0"
+
+class H5ppConan(ConanFile):
     name = "h5pp"
     version = "1.10.0"
     description = "A C++17 wrapper for HDF5 with focus on simplicity"
     homepage = "https://github.com/DavidAce/h5pp"
     author = "DavidAce <aceituno@kth.se>"
-    topics = ("h5pp","hdf5","c++17","data","storage","conan")
+    topics = ("h5pp", "hdf5", "binary", "storage", "header-only", "c++17")
     url = "https://github.com/DavidAce/h5pp"
     license = "MIT"
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake_find_package_multi"
-    requires = "hdf5/1.12.1"
-    build_policy    = "missing"
-    scm = {
-        "type": "git",
-        "url": "https://github.com/DavidAce/h5pp.git",
-        "revision": "auto",
-        "subfolder": "source_subfolder"}
-
-    options = {"with_eigen": [True, False],
-               "with_spdlog": [True, False],
-               "with_fmt":  [True, False]}
-
-    default_options = {"with_eigen": True,
-                       "with_spdlog": True,
-                       "with_fmt"   : True}
-
-    _cmake = None
+    no_copy_source = True
 
     @property
     def _source_subfolder(self):
@@ -46,65 +31,60 @@ class h5ppConan(ConanFile):
             "apple-clang": "10",
         }
 
+    def export_sources(self):
+        self.copy("LICENSE", dst=self._source_subfolder)
+        self.copy("include/*", os.path.join(self._source_subfolder, "include"))
+
     def requirements(self):
-        if self.options.with_eigen:
-            self.requires("eigen/3.4.0")
-        if self.options.with_spdlog:
-            self.requires("spdlog/1.10.0")
-        if self.options.with_fmt:
-            self.requires("fmt/8.1.1")
-
-    def _configure_cmake(self):
-        if not self._cmake:
-            self._cmake = CMake(self)
-            if Version(self.version) >= "1.9.0":
-                self._cmake.definitions["H5PP_PACKAGE_MANAGER"]  = "none"
-            self._cmake.configure(source_folder=self._source_subfolder)
-        return self._cmake
-
-    def validate(self):
-        if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, 17)
-
-    def build(self):
-        cmake = self._configure_cmake()
-        cmake.build()
-
-    def package(self):
-        cmake = self._configure_cmake()
-        cmake.install()
-        print("Copying license")
-        self.copy("LICENSE", src=self._source_subfolder, dst="licenses")
-        if Version(self.version) <= "1.8.6":
-            tools.rmdir(os.path.join(self.package_folder, "share"))
-            self.output.success("package(): Removed directory: share")
-        else:
-            tools.rmdir(os.path.join(self.package_folder, "lib/cmake"))
-            self.output.success("package(): Removed directory: lib/cmake")
-
+        self.requires("eigen/3.4.0")
+        self.requires("hdf5/1.12.1")
+        self.requires("spdlog/1.10.0")
 
     def package_id(self):
         self.info.header_only()
 
-    def package_info(self):
-        self.cpp_info.set_property("cmake_target_name", "h5pp")
-        self.cpp_info.components["h5pp_headers"].set_property("cmake_target_name", "headers")
-        self.cpp_info.components["h5pp_deps"].set_property("cmake_target_name", "deps")
-        self.cpp_info.components["h5pp_flags"].set_property("cmake_target_name", "flags")
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, 17)
+        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+        if minimum_version:
+            if tools.Version(self.settings.compiler.version) < minimum_version:
+                raise ConanInvalidConfiguration("h5pp requires C++17, which your compiler does not support.")
+        else:
+            self.output.warn("h5pp requires C++17. Your compiler is unknown. Assuming it supports C++17.")
 
-        self.cpp_info.components["h5pp_deps"].requires = ["eigen::eigen", "fmt::fmt", "spdlog::spdlog", "hdf5::hdf5"]
+    def package(self):
+        self.copy("LICENSE", src=self._source_subfolder, dst="licenses")
+        self.copy("*.h", src=os.path.join(self._source_subfolder, "include"))
+
+
+    def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "h5pp")
+        self.cpp_info.set_property("cmake_target_name", "h5pp::h5pp")
+        self.cpp_info.components["h5pp_headers"].set_property("cmake_target_name", "h5pp::headers")
         self.cpp_info.components["h5pp_headers"].includedirs = ["include"]
+        self.cpp_info.components["h5pp_deps"].set_property("cmake_target_name", "h5pp::deps")
+        self.cpp_info.components["h5pp_flags"].set_property("cmake_target_name", "h5pp::flags")
+        self.cpp_info.components["h5pp_flags"].defines.append("H5PP_USE_EIGEN3")
+        self.cpp_info.components["h5pp_flags"].defines.append("H5PP_USE_SPDLOG")
+        self.cpp_info.components["h5pp_flags"].defines.append("H5PP_USE_FMT")
+
+        self.cpp_info.components["h5pp_deps"].requires = ["eigen::eigen", "spdlog::spdlog", "hdf5::hdf5"]
         self.cpp_info.components["h5pp_headers"].requires = ["h5pp_deps",  "h5pp_flags"]
 
-        if self.options.with_eigen:
-            self.cpp_info.components["h5pp_flags"].defines.append("H5PP_USE_EIGEN3")
-        if self.options.with_spdlog:
-            self.cpp_info.components["h5pp_flags"].defines.append("H5PP_USE_SPDLOG")
-        if self.options.with_fmt:
-            self.cpp_info.components["h5pp_flags"].defines.append("H5PP_USE_FMT")
-
-        if self.settings.compiler == "gcc" and tools.Version(self.settings.compiler.version) < "9":
+        if (self.settings.compiler == "gcc" and tools.Version(self.settings.compiler.version) < "9") or \
+           (self.settings.compiler == "clang" and self.settings.compiler.get_safe("libcxx") in ["libstdc++", "libstdc++11"]):
             self.cpp_info.components["h5pp_flags"].system_libs = ["stdc++fs"]
-        if self.settings.compiler == "Visual Studio":
-            self.cpp_info.components["h5pp_flags"].defines.append("NOMINMAX")
-            self.cpp_info.components["h5pp_flags"].cxxflags = ["/permissive-", "/EHsc"]
+        if is_msvc(self):
+            self.cpp_info.components["h5pp_flags"].defines = ["NOMINMAX"]
+            self.cpp_info.components["h5pp_flags"].cxxflags = ["/permissive-"]
+
+        # TODO: to remove in conan v2 once cmake_find_package_* generators removed
+        self.cpp_info.names["cmake_find_package"] = "h5pp"
+        self.cpp_info.names["cmake_find_package_multi"] = "h5pp"
+        self.cpp_info.components["h5pp_headers"].names["cmake_find_package"] = "headers"
+        self.cpp_info.components["h5pp_headers"].names["cmake_find_package_multi"] = "headers"
+        self.cpp_info.components["h5pp_deps"].names["cmake_find_package"] = "deps"
+        self.cpp_info.components["h5pp_deps"].names["cmake_find_package_multi"] = "deps"
+        self.cpp_info.components["h5pp_flags"].names["cmake_find_package"] = "flags"
+        self.cpp_info.components["h5pp_flags"].names["cmake_find_package_multi"] = "flags"
