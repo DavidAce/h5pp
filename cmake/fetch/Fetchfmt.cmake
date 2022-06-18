d20976866e37e67c3a931d2836e8d4a88d3b5911
DavidAce <aceituno@kth.se>
2022-06-18 15:11:42 +0200

    Updated dependency versions in CPM and FetchContent automatic build

--- cmake/fetch/Fetchfmt.cmake
+++ cmake/fetch/Fetchfmt.cmake
@@ -8,8 +8,8 @@ function(Fetchfmt)
     option(FMT_CUDA_TEST "Generate the cuda-test target." OFF)
     include(FetchContent)
     FetchContent_Declare(fetch-fmt
-            URL         https://github.com/fmtlib/fmt/archive/8.0.1.tar.gz
-            URL_MD5     7d5af964c6633ef90cd6a47be3afe6a0
+            URL         https://github.com/fmtlib/fmt/archive/8.1.1.tar.gz
+            URL_MD5     fe7f1585844b14c647bd332ad5562832
             )
     FetchContent_MakeAvailable(fetch-fmt)
     find_package(fmt REQUIRED)
