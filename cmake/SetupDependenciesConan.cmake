8c005f77c2802b3f8320755a08c3639c1e818cd3
DavidAce <aceituno@kth.se>
2022-06-18 15:14:24 +0200

    Started using CMakeDeps generator for conan

--- cmake/SetupDependenciesConan.cmake
+++ cmake/SetupDependenciesConan.cmake
@@ -21,7 +21,7 @@ if(H5PP_PACKAGE_MANAGER MATCHES "conan")
     conan_cmake_install(
             CONAN_COMMAND ${CONAN_COMMAND}
             BUILD missing outdated cascade
-            GENERATOR cmake_find_package_multi
+            GENERATOR CMakeDeps
             SETTINGS ${CONAN_AUTODETECT}
             INSTALL_FOLDER ${CMAKE_BINARY_DIR}/conan
             PATH_OR_REFERENCE ${CMAKE_SOURCE_DIR}
@@ -38,8 +38,8 @@ if(H5PP_PACKAGE_MANAGER MATCHES "conan")
     list(PREPEND CMAKE_MODULE_PATH ${CMAKE_BINARY_DIR}/conan)
     # Use CONFIG to avoid MODULE mode. This is recommended for the cmake_find_package_multi generator
     find_package(HDF5 1.12.1 COMPONENTS C HL REQUIRED CONFIG)
-    find_package(Eigen3 3.4 REQUIRED CONFIG)
+    find_package(Eigen 3.4 REQUIRED CONFIG)
     find_package(spdlog 1.10.0 REQUIRED CONFIG)
     find_package(fmt 8.1.1 REQUIRED CONFIG)
-    target_link_libraries(deps INTERFACE HDF5::HDF5 Eigen3::Eigen spdlog::spdlog fmt::fmt)
+    target_link_libraries(deps INTERFACE hdf5::hdf5 eigen::eigen spdlog::spdlog fmt::fmt)
 endif()
