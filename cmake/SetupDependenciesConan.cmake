5b41a880bf129838b8fcfce9ba3d8402fcba70ba
DavidAce <aceituno@kth.se>
2022-09-16 12:49:01 +0200

    Updated cmake-conan integrator

--- cmake/SetupDependenciesConan.cmake
+++ cmake/SetupDependenciesConan.cmake
@@ -10,9 +10,9 @@ if(H5PP_PACKAGE_MANAGER MATCHES "conan")
     # Download cmake-conan integrator
     if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan/conan.cmake")
         message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
-        file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/release/0.17/conan.cmake"
+        file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/0.18.1/conan.cmake"
                 "${CMAKE_BINARY_DIR}/conan/conan.cmake"
-                EXPECTED_HASH MD5=52a255a933397fdce3d0937f9c737e98
+                EXPECTED_HASH MD5=81d5eab13a49f43527e35a90bfac6960
                 TLS_VERIFY ON)
     endif()
 
@@ -44,11 +44,10 @@ if(H5PP_PACKAGE_MANAGER MATCHES "conan")
     ##################################################################
     ### Find all the things!                                       ###
     ##################################################################
-    if(NOT CONAN_CMAKE_SILENT_OUTPUT)
-        set(CONAN_CMAKE_SILENT_OUTPUT OFF) # Default is off
-    endif()
     list(PREPEND CMAKE_PREFIX_PATH ${CMAKE_BINARY_DIR}/conan)
     list(PREPEND CMAKE_MODULE_PATH ${CMAKE_BINARY_DIR}/conan)
+    list(REMOVE_DUPLICATES CMAKE_MODULE_PATH)
+    list(REMOVE_DUPLICATES CMAKE_PREFIX_PATH)
     # Use CONFIG to avoid MODULE mode. This is recommended for the cmake_find_package_multi generator
     find_package(HDF5 1.13.1 COMPONENTS C HL REQUIRED CONFIG)
     find_package(Eigen3 3.4 REQUIRED CONFIG)
