bba53545a09e86f0aa656cba97619c2d80d3a438
DavidAce <aceituno@kth.se>
2023-03-28 18:35:54 +0200

    Fixed chunk dimension test that wasn't actually testing chunking

--- tests/test-chunkDimension.cpp
+++ tests/test-chunkDimension.cpp
@@ -11,6 +11,11 @@ int main() {
     using namespace std::complex_literals;
     std::vector<std::complex<double>> vectorComplexDouble(10000, {10.0, 5.0});
 
-    file.writeDataset(vectorComplexDouble, "chunkedgroup/vectorComplexDouble");
+    file.writeDataset(vectorComplexDouble, "chunkedgroup/vectorComplexDouble", H5D_CHUNKED);
+
+    auto chunkDims = file.getDatasetChunkDimensions("chunkedgroup/vectorComplexDouble");
+
+    if(not chunkDims) throw h5pp::runtime_error("Failed to enable chunking on dataset: {}", "chunkedgroup/vectorComplexDouble");
+    else h5pp::logger::log->info("Created dataset chunkedgroup/vectorComplexDouble with chunk dimensions {}", chunkDims.value());
     return 0;
 }
